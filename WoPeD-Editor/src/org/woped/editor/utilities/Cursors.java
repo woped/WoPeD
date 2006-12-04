905b7b49dba32130ee2dd9cbeaedbdfb05069df0
smellert <smellert>
2006-12-04 23:03:41 +0000

    removed useless (auto generated) TODO comments

--- WoPeD-Editor/src/org/woped/editor/utilities/Cursors.java
+++ WoPeD-Editor/src/org/woped/editor/utilities/Cursors.java
@@ -1,8 +1,5 @@
 /*
  * Created on 20.10.2005
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 package org.woped.editor.utilities;
 
@@ -17,9 +14,6 @@ import org.woped.core.model.petrinet.PetriNetModelElement;
 
 /**
  * @author lai
- * 
- * TODO To change the template for this generated type comment go to Window -
- * Preferences - Java - Code Style - Code Templates
  */
 public class Cursors
 {
