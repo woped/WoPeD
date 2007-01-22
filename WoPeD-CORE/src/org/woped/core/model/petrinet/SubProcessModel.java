ec72e2cf94e49728aa9f01805e6efaf16359ba4d
assall <assall>
2007-01-22 11:24:13 +0000

    perparation for copy/past of subprocesses

--- WoPeD-CORE/src/org/woped/core/model/petrinet/SubProcessModel.java
+++ WoPeD-CORE/src/org/woped/core/model/petrinet/SubProcessModel.java
@@ -23,6 +23,8 @@
 package org.woped.core.model.petrinet;
 
 import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 import org.jgraph.graph.DefaultPort;
 import org.jgraph.graph.Edge;
@@ -40,65 +42,179 @@ import org.woped.core.model.ModelElementFactory;
  */
 
 @SuppressWarnings("serial")
-public class SubProcessModel extends TransitionModel implements InnerElementContainer
+public class SubProcessModel extends TransitionModel implements
+		InnerElementContainer
 {
 
-    public static final int WIDTH  = 40;
-    public static final int HEIGHT = 40;
+	public static final int WIDTH = 40;
+	public static final int HEIGHT = 40;
+	private int subElementCounter = 0;
+	private ModelElementContainer subElementContainer;
+	private static final String SUBELEMENT_SEPERATOR = "_";
 
-    public SubProcessModel(CreationMap map)
-    {
-        //super(jGraphModel);
-        super(map);
-        setSize(WIDTH, HEIGHT);
-        getToolSpecific().setSubprocess(true);
-        
-        // The sub element container
-        // is owned by the subprocess
-        subElementContainer.setOwningElement(this);
-                
-    }
+	public SubProcessModel(CreationMap map)
+	{
+		super(map);
+		setSize(WIDTH, HEIGHT);
+		getToolSpecific().setSubprocess(true);
 
-    public String getToolTipText()
-    {
-        return "Subprocess\nID: " + getId() + "\nName: " + getNameValue() + "\nSubprocess feature is not implemented, yet.";
-    }
+		//there is already a container -> make a copy
+		if (map.getSubElementContainer() != null)
+		{
+			ModelElementContainer newContainer = copySubElementContainer(map
+					.getSubElementContainer());
+			newContainer.setOwningElement(this);
+			subElementContainer = newContainer;
+		} 
+		else
+		{
+			// The sub element container
+			// is owned by the subprocess
+			subElementContainer = new ModelElementContainer();
+			subElementContainer.setOwningElement(this);
+		}
+		
+	}
 
-    public int getType()
-    {
-        return PetriNetModelElement.SUBP_TYPE;
-    }
-    
-    private int                   subElementCounter    = 0;
-    private ModelElementContainer subElementContainer  = new ModelElementContainer();
-    private static final String    SUBELEMENT_SEPERATOR     = "_";
-        
-    public ModelElementContainer getSimpleTransContainer()
-    {
-    	return subElementContainer;
-    }
+	private ModelElementContainer copySubElementContainer(
+			ModelElementContainer container)
+	{
+		System.out.println(">>>>>>>>>>>>>>>>>>>>>>cloneSubElementContainer");
+		ModelElementContainer newContainer = null;
+
+		newContainer = new ModelElementContainer();
+
+		// copy elements
+		{
+			Map idMap = container.getIdMap();
+			Set idMapKeys = idMap.keySet();
+			Iterator keyIterator = idMapKeys.iterator();
+
+			while (keyIterator.hasNext())
+			{
+				AbstractElementModel currentElement = (AbstractElementModel) container
+						.getElementById(keyIterator.next());
+				System.out.println(currentElement.getId() + ": "
+						+ currentElement.getNameValue());
+				CreationMap newMap = (CreationMap) currentElement
+						.getCreationMap().clone();
+				newMap.setId("copyof_" + newMap.getId());
+				newMap.setName("copyof_" + newMap.getName());
+
+				if (newMap.getType() == PetriNetModelElement.TRANS_SIMPLE_TYPE)
+				{
+					AbstractElementModel newElement = new TransitionModel(
+							newMap);
+					newContainer.addElement(newElement);
+				} else if (newMap.getType() == PetriNetModelElement.PLACE_TYPE)
+				{
+
+					AbstractElementModel newElement = new PlaceModel(newMap);
+					newContainer.addElement(newElement);
+
+				}
+
+			}
+		}
+
+		// copy arcs
+//		{
+//			Map arcMap = container.getArcMap();
+//			Map newArcMap = new HashMap();
+//			Set arcMapKeys = arcMap.keySet();
+//			Iterator arcIterator = arcMapKeys.iterator();
+//
+//			int counter = 1;
+//
+//			while (arcIterator.hasNext())
+//			{
+//				ArcModel currentArcModell = (ArcModel) arcMap.get(arcIterator
+//						.next());
+//
+//				CreationMap arcCreationMap = (CreationMap) currentArcModell
+//						.getCreationMap().clone();
+//
+//				arcCreationMap.setArcSourceId("copyof_"
+//						+ arcCreationMap.getArcSourceId());
+//				arcCreationMap.setArcTargetId("copyof_"
+//						+ arcCreationMap.getArcTargetId());
+//				arcCreationMap.setArcId("copyof_" + arcCreationMap.getArcId());
+//
+//				System.out.println(":::::::::::::createArc");
+//				ArcModel newArc = new ArcModel(arcCreationMap);
+//				System.out.println(":::::::::::::addArc");
+//				// newContainer.addReference(newArc);
+//
+//				newArcMap.put("a" + counter++, newArc);
+//			}
+//
+//			newContainer.setArcMap(newArcMap);
+//		}
+
+		// remove old input and output
+		// they are read-only...
+		Map places = newContainer
+				.getElementsByType(PetriNetModelElement.PLACE_TYPE);
+
+		Iterator placeKeyIterator = places.keySet().iterator();
+
+		while (placeKeyIterator.hasNext())
+		{
+			PlaceModel currentPlace = (PlaceModel) places.get(placeKeyIterator
+					.next());
+
+			if (currentPlace.isReadOnly())
+			{
+				newContainer.removeElement(currentPlace.getId());
+			}
+
+		}
+
+		return newContainer;
+	}
 	
-    public AbstractElementModel addElement(AbstractElementModel element)
-    {
-    	return subElementContainer.addElement(element);    	
-    }
-    public void addReference(String arcId, DefaultPort sourceId, DefaultPort targetId)
-    {
-        subElementContainer.addReference(ModelElementFactory.createArcModel(arcId, sourceId, targetId));
-    }
-    public AbstractElementModel getElement(Object elementId)
-    {
-    	return subElementContainer.getElementById(elementId);
-    }
-    public String getNewElementId()
-    {
-        subElementCounter++;
-        return getId() + SUBELEMENT_SEPERATOR + subElementCounter;    	
-    }
-    
-    //! Overwritten to allow new outgoing connections only
-    //! if there is not already an outgoing connection 
-    //! (Sub-processes must have exactly one input and one output)
+	public String getToolTipText()
+	{
+		return "Subprocess\nID: " + getId() + "\nName: " + getNameValue()
+				+ "\nSubprocess feature is not implemented, yet.";
+	}
+
+	public int getType()
+	{
+		return PetriNetModelElement.SUBP_TYPE;
+	}
+
+	public ModelElementContainer getSimpleTransContainer()
+	{
+		return subElementContainer;
+	}
+
+	public AbstractElementModel addElement(AbstractElementModel element)
+	{
+		return subElementContainer.addElement(element);
+	}
+
+	public void addReference(String arcId, DefaultPort sourceId,
+			DefaultPort targetId)
+	{
+		subElementContainer.addReference(ModelElementFactory.createArcModel(
+				arcId, sourceId, targetId));
+	}
+
+	public AbstractElementModel getElement(Object elementId)
+	{
+		return subElementContainer.getElementById(elementId);
+	}
+
+	public String getNewElementId()
+	{
+		subElementCounter++;
+		return getId() + SUBELEMENT_SEPERATOR + subElementCounter;
+	}
+
+	// ! Overwritten to allow new outgoing connections only
+	// ! if there is not already an outgoing connection
+	// ! (Sub-processes must have exactly one input and one output)
 	public boolean getAllowOutgoingConnections()
 	{
 		boolean result = true;
@@ -107,14 +223,14 @@ public class SubProcessModel extends TransitionModel implements InnerElementCont
 		{
 			Object o = i.next();
 			if (o instanceof Edge)
-			{        				
-				Edge e = (Edge)o;
-				if (e.getSource()==getPort())
+			{
+				Edge e = (Edge) o;
+				if (e.getSource() == getPort())
 					++nNumOutgoing;
 			}
 		}
 		result = (nNumOutgoing == 0);
 		return result;
 	}
-  
+
 }
