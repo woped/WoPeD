c93ab72b33e9dffb6ba6d15d880a7d7a7a06d02a
smellert <smellert>
2007-01-28 19:44:26 +0000

    organized import

--- WoPeD-CORE/src/org/woped/core/model/petrinet/SubProcessModel.java
+++ WoPeD-CORE/src/org/woped/core/model/petrinet/SubProcessModel.java
@@ -24,7 +24,6 @@ package org.woped.core.model.petrinet;
 
 import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
 
 import org.jgraph.graph.DefaultPort;
 import org.jgraph.graph.Edge;
