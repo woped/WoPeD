801c5dd792f1c7048b01284acb57b96d3398618a
cczech <cczech>
2007-04-05 11:47:04 +0000

    Umbenennung von CapacityPlan Start-Klasse f�r quant. Analyse

---
+++ WoPeD-QuantAnalysis/org/woped/quantanalysis/QuantAnaStart.java
@@ -0,0 +1,29 @@
+package org.woped.quantanalysis;
+
+import org.woped.core.model.ModelElementContainer;
+import org.woped.core.utilities.LoggerManager;
+import org.woped.editor.controller.vc.EditorVC;
+
+public class QuantAnaStart {
+
+	EditorVC editor = null;
+	ModelElementContainer mec = null;
+	QuantitativeAnalysisDialog qa = null;
+	
+	/**
+	 * @param args
+	 */
+	public QuantAnaStart(EditorVC editor) {
+		this.editor = editor;
+		mec = editor.getModelProcessor().getElementContainer();
+		LoggerManager.info(Constants.QUANTANA_LOGGER, "Quantitative Analyse läuft.");
+		
+		qa = new QuantitativeAnalysisDialog(null ,true, editor);
+		qa.setVisible(true);
+	}
+	
+	public void message(String msg)
+	{
+		System.out.println(msg);
+	}
+}
