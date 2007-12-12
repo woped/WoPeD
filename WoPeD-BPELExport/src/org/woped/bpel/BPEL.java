af677328ee0cae5d4e1efd04c3fd3e685950274f
tobias1981 <tobias1981>
2007-12-12 18:33:58 +0000

    BPEL-Export now uses XMLBeans; Function is just experimental yet

--- WoPeD-BPELExport/src/org/woped/bpel/BPEL.java
+++ WoPeD-BPELExport/src/org/woped/bpel/BPEL.java
@@ -1,6 +1,9 @@
 package org.woped.bpel;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Vector;
 
 import javax.swing.JFileChooser;
@@ -10,6 +13,11 @@ import org.woped.core.model.PetriNetModelProcessor;
 import org.woped.core.utilities.Utils;
 import org.woped.editor.utilities.FileFilterImpl;
 
+import org.apache.xmlbeans.XmlCursor;
+import org.apache.xmlbeans.XmlOptions;
+import org.woped.bpel.ProcessDocument;
+import org.woped.bpel.TProcess;
+
 
 //TODO class description
 public class BPEL {
@@ -17,6 +25,8 @@ public class BPEL {
 	private static BPEL bpelMainClass;
 
 	private Vector<String> _extensions;
+
+	private ProcessDocument bpelDoc;
 
 	private FileFilter _filter;
 
@@ -64,8 +74,27 @@ public class BPEL {
 	//TODO method description
 	public boolean saveFile(String Path, PetriNetModelProcessor pnp)
 	{
-		new File(Path);
-		return true;
+		//new File(Path);
+		bpelDoc = ProcessDocument.Factory.newInstance();
+		TProcess iProcess = bpelDoc.addNewProcess();
+		XmlCursor cursor = iProcess.newCursor();
+		cursor.insertComment("Hello World");
+		
+        XmlOptions opt = new XmlOptions();
+        opt.setUseDefaultNamespace();
+        opt.setSavePrettyPrint();
+        opt.setSavePrettyPrintIndent(2);
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("", "bpel.woped.org");
+        opt.setSaveImplicitNamespaces(map);
+        try
+        {
+        	bpelDoc.save(new File(Path), opt);
+        	return true;
+        } catch (IOException e)
+        {
+            return false;
+        }
 	}
 
 }
