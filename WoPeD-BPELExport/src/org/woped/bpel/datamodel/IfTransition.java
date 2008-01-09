b50475d2b3c3bcd25b86fff663e001f2e328bfca
joh_h <joh_h>
2008-01-09 11:38:11 +0000

    update getBPELCode()

--- WoPeD-BPELExport/src/org/woped/bpel/datamodel/IfTransition.java
+++ WoPeD-BPELExport/src/org/woped/bpel/datamodel/IfTransition.java
@@ -1,13 +1,19 @@
 package org.woped.bpel.datamodel;
 
+import java.util.Iterator;
+
 import org.oasisOpen.docs.wsbpel.x20.process.executable.TActivity;
+import org.oasisOpen.docs.wsbpel.x20.process.executable.TBooleanExpr;
+import org.oasisOpen.docs.wsbpel.x20.process.executable.TIf;
 
 public class IfTransition extends TerminalElement
 {
+	private AbstractElement begin;
 
 	public IfTransition(AbstractElement begin)
 	{
 		super("test");
+		this.begin = begin;
 	}
 
 	@Override
@@ -21,7 +27,20 @@ public class IfTransition extends TerminalElement
 	@Override
 	public TActivity getBpelCode()
 	{
-		return null;
+		AbstractElement tmp = null;
+		TIf iIf = null;
+		Iterator<AbstractElement> list = begin.get_all_post_objects().iterator();
+		while (list.hasNext())
+		{
+			tmp = list.next();			
+			
+			for (int i = 0; i < 3; i++)
+			{					
+				//transition case
+				tmp = tmp.get_first_post_element();
+			}			
+		}
+		return iIf;
 	}
 
 }
