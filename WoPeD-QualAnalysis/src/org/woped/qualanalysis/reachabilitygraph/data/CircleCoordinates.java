f716f810271e6c6502f8bcbe429f0b731dc33e83
horscht1985 <horscht1985>
2008-01-16 14:55:21 +0000

    Inserted Method to calculate the coordinates
    M. Fladt

--- WoPeD-QualAnalysis/src/org/woped/qualanalysis/reachabilitygraph/data/CircleCoordinates.java
+++ WoPeD-QualAnalysis/src/org/woped/qualanalysis/reachabilitygraph/data/CircleCoordinates.java
@@ -4,10 +4,20 @@ import java.awt.Point;
 import java.util.LinkedList;
 
 public class CircleCoordinates {
-	public static LinkedList<Point> getCircleCoordinates(int width, int height, int objectcount){
+	public static LinkedList<Point> getCircleCoordinates(int width, int height, int objectcount) {
 		LinkedList<Point> points = new LinkedList<Point>();
-		for(int i = 0; i < objectcount; i++){
-			points.add(new Point(5*i,5*i));
+		// get the Middle of the graph and do some correction to fit correctly
+		int mwidth = (width / 2) - 60;
+		int mheight = (height / 2) - 45;
+
+		// get the steps in degrees
+		int degreestep = 360 / objectcount;
+		
+		//Fill the linked list with coordinates for every object
+		for(int i = 0; i < objectcount; i++) {
+			int x = (int) (mwidth + (mwidth * (Math.sin(Math.toRadians(i * degreestep)))) + 5);
+			int y = (int) (mheight - (mheight * (Math.cos(Math.toRadians(i * degreestep)))) + 5);
+			points.add(new Point(x, y));
 		}
 		return points;
 	}
