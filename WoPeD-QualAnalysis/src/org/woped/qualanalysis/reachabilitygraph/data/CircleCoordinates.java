70ccd8eec19d15d4aa1994ead2214b6531bdb895
jdcspro <jdcspro>
2008-01-16 10:45:49 +0000

    HelperClass for circle layout

--- WoPeD-QualAnalysis/src/org/woped/qualanalysis/reachabilitygraph/data/CircleCoordinates.java
+++ WoPeD-QualAnalysis/src/org/woped/qualanalysis/reachabilitygraph/data/CircleCoordinates.java
@@ -4,7 +4,7 @@ import java.awt.Point;
 import java.util.LinkedList;
 
 public class CircleCoordinates {
-	public static LinkedList<Point> getCircleCoordinates(){
+	public static LinkedList<Point> getCircleCoordinates(int width, int height, int objectcount){
 		LinkedList<Point> points=new LinkedList<Point>();
 		Point punkt1=new Point(10,20);
 		return points;
