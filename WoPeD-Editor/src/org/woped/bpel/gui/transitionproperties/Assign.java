db345f84f198fee7f914f866f7d9e4239b031449
alexanderrossi <alexanderrossi>
2008-01-16 13:37:34 +0000

    get and set methods for AssignPanel

--- WoPeD-Editor/src/org/woped/bpel/gui/transitionproperties/Assign.java
+++ WoPeD-Editor/src/org/woped/bpel/gui/transitionproperties/Assign.java
@@ -1,12 +1,45 @@
 package org.woped.bpel.gui.transitionproperties;
 
+import org.oasisOpen.docs.wsbpel.x20.process.executable.TAssign;
+import org.oasisOpen.docs.wsbpel.x20.process.executable.TCopy;
+import org.oasisOpen.docs.wsbpel.x20.process.executable.TFrom;
+import org.oasisOpen.docs.wsbpel.x20.process.executable.TInvoke;
+import org.oasisOpen.docs.wsbpel.x20.process.executable.TProcess;
+import org.oasisOpen.docs.wsbpel.x20.process.executable.TTo;
+
 
 public class Assign extends BaseActivity
 {
 	
 	public Assign()
 	{
-		// TODO Auto-generated constructor stub
+		
+	}
+	
+	public void saveInformation(BPELassignPanel bip)
+	{
+		TProcess p = BaseActivity.genBpelProsses();
+		TAssign assign = p.addNewAssign();
+		TCopy copy = assign.addNewCopy();
+		TFrom from = copy.addNewFrom();
+		//from.setVariable(bip.getFromVariable());
+		
+		TTo to = copy.addNewTo();
+		//to.setVariable(bip.getToVariable());
+		this.setActivity(assign);
+	}
+	
+	public void setInformation(BPELassignPanel bip)
+	{
+		TAssign assign = (TAssign) this.getActivity();
+		TCopy copy = assign.getCopyArray(0);
+		TFrom from = copy.getFrom();
+		String sFromVariable = from.getVariable();
+		//bip.setFromVariable(sFromVariable);
+		
+		TTo to = copy.getTo();
+		String sToVariable = to.getVariable();
+		//bip.setToVariable(sToVariable);	
 	}
 
 }
