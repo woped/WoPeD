a0b815703538c4295e5936bc05e38c6920889dd7
tobias1981 <tobias1981>
2008-01-16 10:43:48 +0000

    BPEL-If added

--- WoPeD-BPELExport/src/org/woped/bpel/datamodel/IfTransition.java
+++ WoPeD-BPELExport/src/org/woped/bpel/datamodel/IfTransition.java
@@ -3,8 +3,18 @@ package org.woped.bpel.datamodel;
 import java.util.Iterator;
 
 import org.oasisOpen.docs.wsbpel.x20.process.executable.TActivity;
+import org.oasisOpen.docs.wsbpel.x20.process.executable.TAssign;
 import org.oasisOpen.docs.wsbpel.x20.process.executable.TBooleanExpr;
+import org.oasisOpen.docs.wsbpel.x20.process.executable.TEmpty;
+import org.oasisOpen.docs.wsbpel.x20.process.executable.TFlow;
 import org.oasisOpen.docs.wsbpel.x20.process.executable.TIf;
+import org.oasisOpen.docs.wsbpel.x20.process.executable.TInvoke;
+import org.oasisOpen.docs.wsbpel.x20.process.executable.TPick;
+import org.oasisOpen.docs.wsbpel.x20.process.executable.TReceive;
+import org.oasisOpen.docs.wsbpel.x20.process.executable.TReply;
+import org.oasisOpen.docs.wsbpel.x20.process.executable.TSequence;
+import org.oasisOpen.docs.wsbpel.x20.process.executable.TWait;
+import org.woped.bpel.BPEL;
 
 public class IfTransition extends TerminalElement
 {
@@ -28,18 +38,57 @@ public class IfTransition extends TerminalElement
 	public TActivity getBpelCode()
 	{
 		AbstractElement tmp = null;
-		TIf iIf = null;
+		TIf iIf = BPEL.genBpelProsses().addNewIf();
 		Iterator<AbstractElement> list = begin.get_all_post_objects().iterator();
 		while (list.hasNext())
 		{
-			tmp = list.next();			
+			tmp = list.next();
+			tmp=tmp.get_first_post_element();			
 			
-			for (int i = 0; i < 3; i++)
-			{					
-				//transition case
-				tmp = tmp.get_first_post_element();
+			iIf.	
+			//transition case
+				//tmp = tmp.get_first_post_element();
+				if(TPick.class.isInstance(tmp.getBpelCode())){
+					TPick iPick = iIf.addNewPick();
+					iPick.set(tmp.getBpelCode());			
+				}
+				else if(TSequence.class.isInstance(tmp.getBpelCode())){
+					TSequence iSeq = iIf.addNewSequence();
+					iSeq.set(tmp.getBpelCode());
+				}
+				else if(TIf.class.isInstance(tmp.getBpelCode())){
+					TIf iIfSub = iIf.addNewIf();
+					iIfSub.set(tmp.getBpelCode());
+				}
+				else if(TAssign.class.isInstance(tmp.getBpelCode())){
+					TAssign iAss = iIf.addNewAssign();
+					iAss.set(tmp.getBpelCode());			
+				}
+				else if(TFlow.class.isInstance(tmp.getBpelCode())){
+					TFlow iFlow = iIf.addNewFlow();
+					iFlow.set(tmp.getBpelCode());
+				}
+				else if(TEmpty.class.isInstance(tmp.getBpelCode())){
+					TEmpty iEmpty = iIf.addNewEmpty();
+					iEmpty.set(tmp.getBpelCode());
+				}
+				else if(TWait.class.isInstance(tmp.getBpelCode())){
+					TWait iWait = iIf.addNewWait();
+					iWait.set(tmp.getBpelCode());
+				}
+				else if(TReceive.class.isInstance(tmp.getBpelCode())){
+					TReceive iReceive = iIf.addNewReceive();
+					iReceive.set(tmp.getBpelCode());
+				}
+				else if(TReply.class.isInstance(tmp.getBpelCode())){
+					TReply iReply = iIf.addNewReply();
+					iReply.set(tmp.getBpelCode());
+				}
+				else if(TInvoke.class.isInstance(tmp.getBpelCode())){
+					TInvoke iInvoke = iIf.addNewInvoke();
+					iInvoke.set(tmp.getBpelCode());
+				}
 			}			
-		}
 		return iIf;
 	}
 
