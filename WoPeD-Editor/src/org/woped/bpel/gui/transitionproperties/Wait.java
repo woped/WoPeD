db07c90d2e9912ad016cf0f4cdade50143287796
alexanderrossi <alexanderrossi>
2008-01-16 22:26:18 +0000

    *** empty log message ***

--- WoPeD-Editor/src/org/woped/bpel/gui/transitionproperties/Wait.java
+++ WoPeD-Editor/src/org/woped/bpel/gui/transitionproperties/Wait.java
@@ -1,5 +1,9 @@
 package org.woped.bpel.gui.transitionproperties;
 
+import org.oasisOpen.docs.wsbpel.x20.process.executable.TProcess;
+import org.oasisOpen.docs.wsbpel.x20.process.executable.TReply;
+import org.oasisOpen.docs.wsbpel.x20.process.executable.TWait;
+
 
 public class Wait extends BaseActivity
 {
@@ -11,12 +15,33 @@ public class Wait extends BaseActivity
 	
 	public void saveInformation(BPELwaitPanel bwp)
 	{
-		//Problem Unterscheidung zwischen For und Until, muss in GUI abgefragt werden
+		//Problem Unterscheidung zwischen For und Until
+		TProcess p = BaseActivity.genBpelProsses();
+		TWait wait = p.addNewWait();
+		
+		/*if(bwp.getFor() != null)
+		{
+			wait.setFor(bwp.getFor());
+		}
+		if(bwp.getUntil() != null)
+		{
+			wait.setUntil(bwp.getUntil());
+		}*/
 	}
 	
 	public void setInformation(BPELwaitPanel bwp)
 	{
-		//Problem Unterscheidung zwischen For und Until, muss in GUI abgefragt werden
+		//Problem Unterscheidung zwischen For und Until
+		TWait wait = (TWait) this.getActivity();
+		
+		/*if(wait.getFor() != null)
+		{
+			bwp.setFor(wait.getFor());
+		}
+		if(wait.getUntil() != null)
+		{
+			bwp.setUntil(wait.getUntil());
+		}*/
 	}
 
 }
