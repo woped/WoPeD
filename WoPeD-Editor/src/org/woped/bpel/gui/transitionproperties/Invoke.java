70751d6ceac23e67a2896f187feec5cd28bb609a
alexanderrossi <alexanderrossi>
2008-01-16 14:25:14 +0000

    *** empty log message ***

--- WoPeD-Editor/src/org/woped/bpel/gui/transitionproperties/Invoke.java
+++ WoPeD-Editor/src/org/woped/bpel/gui/transitionproperties/Invoke.java
@@ -1,9 +1,13 @@
 package org.woped.bpel.gui.transitionproperties;
 
+import org.oasisOpen.docs.wsbpel.x20.process.executable.TAssign;
+import org.oasisOpen.docs.wsbpel.x20.process.executable.TInvoke;
+import org.oasisOpen.docs.wsbpel.x20.process.executable.TProcess;
+
 
 public class Invoke extends BaseActivity
 {
-
+	
 	public Invoke()
 	{
 		// TODO Auto-generated constructor stub
@@ -11,7 +15,34 @@ public class Invoke extends BaseActivity
 	
 	public void saveInformation(BPELinvokePanel bip)
 	{
+		//erzeuge XML-Baum
+		TProcess p = BaseActivity.genBpelProsses();
+		TInvoke invoke = p.addNewInvoke();
+		//invoke.setPartnerLink(bip.getPartnerLink());
+		//invoke.setOperation(bip.getOperation());
+		//invoke.setPortType(bip.getPortType());????
+		//invoke.setInputVariable(bip.getInputVariable());
+		//invoke.setOutputVariable(bip.getInputVariable());
+	}
+	
+	public void setInformation(BPELinvokePanel bip)
+	{
+		TInvoke invoke = (TInvoke) this.getActivity();
 		
+		String sPartnerLink = invoke.getPartnerLink();
+		//bip.setPartnerLink(sPartnerLink);
+		
+		String sOperation = invoke.getOperation();
+		//bip.setOperation(sOperation);
+		
+		//String sPortType = invoke.getPortType();
+		//bip.getPortType(sPortType);
+		
+		String sInputVariable = invoke.getInputVariable();
+		//bip.setInputVariable(sInputVariable);
+		
+		String sOutputVariable = invoke.getOutputVariable();
+		//bip.setOutputVariable(sOutputVariable);
 	}
 
 }
