0437475dea8a40b772acaf20fd814c31a2a574ea
horscht1985 <horscht1985>
2008-01-16 10:21:44 +0000

    Creation of class to get the coordinates of objects on a circle
    M. Fladt

---
+++ WoPeD-QualAnalysis/src/org/woped/qualanalysis/reachabilitygraph/data/CircleCoordinates.java
@@ -0,0 +1,12 @@
+package org.woped.qualanalysis.reachabilitygraph.data;
+
+import java.awt.Point;
+import java.util.LinkedList;
+
+public class CircleCoordinates {
+	public static LinkedList<Point> getCircleCoordinates(){
+		LinkedList<Point> points=new LinkedList<Point>();
+		Point punkt1=new Point(10,20);
+		return points;
+	}
+}
