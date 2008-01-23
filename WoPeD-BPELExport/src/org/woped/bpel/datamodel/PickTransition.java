5d48f050c5ee8a87e3cc935563a839db48b6b137
frankschueler <frankschueler>
2008-01-23 16:17:37 +0000

    *** empty log message ***

--- WoPeD-BPELExport/src/org/woped/bpel/datamodel/PickTransition.java
+++ WoPeD-BPELExport/src/org/woped/bpel/datamodel/PickTransition.java
@@ -9,8 +9,9 @@ import org.woped.bpel.BPEL;
 
 public class PickTransition extends TerminalElement
 {
-	HashSet<AbstractElement> begin;
-	TPick pick = null;
+
+	HashSet<AbstractElement>	begin;
+	TPick						pick	= null;
 
 	public PickTransition(HashSet<AbstractElement> begin)
 	{
@@ -28,178 +29,129 @@ public class PickTransition extends TerminalElement
 	}
 
 	public TActivity getBpelCode()
-	{		
+	{
 		TPick iPick = BPEL.genBpelProsses().addNewPick();
 		XmlObject triggerTransition = null;
 		AbstractElement tmp;
-		Iterator<AbstractElement> list = begin.iterator();		
-		
+		Iterator<AbstractElement> list = begin.iterator();
+
 		while (list.hasNext())
 		{
 			tmp = list.next();
-			System.out.println(tmp);
-			
 
-			//1.element
-			if (TimeTriggerTransition.class.isInstance(tmp)){
-					TOnAlarmPick iOnAlarmPick = iPick.addNewOnAlarm();
-				
-				//transition case (1.transition)			
-				if(TPick.class.isInstance(tmp.getBpelCode())){
-					TPick iPickSub = iOnAlarmPick.addNewPick();
-					iPickSub.set(tmp.getBpelCode());			
-				}
-				else if(TSequence.class.isInstance(tmp.getBpelCode())){
-					TSequence iSeq = iOnAlarmPick.addNewSequence();
-					iSeq.set(tmp.getBpelCode());
-				}
-				else if(TIf.class.isInstance(tmp.getBpelCode())){
-					TIf iIf = iOnAlarmPick.addNewIf();
-					iIf.set(tmp.getBpelCode());
-				}
-				else if(TAssign.class.isInstance(tmp.getBpelCode())){
-					TAssign iAss = iOnAlarmPick.addNewAssign();
-					iAss.set(tmp.getBpelCode());			
-				}
-				else if(TFlow.class.isInstance(tmp.getBpelCode())){
-					TFlow iFlowSub = iOnAlarmPick.addNewFlow();
-					iFlowSub.set(tmp.getBpelCode());
-				}		
-				else if(TEmpty.class.isInstance(tmp.getBpelCode())){
-					TEmpty iEmpty = iOnAlarmPick.addNewEmpty();
-					iEmpty.set(tmp.getBpelCode());
-				}
-				else if(TWait.class.isInstance(tmp.getBpelCode())){
-					TWait iWait = iOnAlarmPick.addNewWait();
-					iWait.set(tmp.getBpelCode());
-				}
-				else if(TReceive.class.isInstance(tmp.getBpelCode())){
-					TReceive iReceive = iOnAlarmPick.addNewReceive();
-					iReceive.set(tmp.getBpelCode());
-				}
-				else if(TReply.class.isInstance(tmp.getBpelCode())){
-					TReply iReply = iOnAlarmPick.addNewReply();
-					iReply.set(tmp.getBpelCode());
-				}
-				else if(TInvoke.class.isInstance(tmp.getBpelCode())){
-					TInvoke iInvoke = iOnAlarmPick.addNewInvoke();
-					iInvoke.set(tmp.getBpelCode());
-				}
+			// 1.element
+			if (TimeTriggerTransition.class.isInstance(tmp))
+			{
+				TOnAlarmPick iOnAlarmPick = iPick.addNewOnAlarm();
+
+				// transition case (1.transition)
+				tmp = tmp.get_first_post_element();
+				tmp = tmp.get_first_post_element();
+				this.addToOnAlarm(iOnAlarmPick, tmp);
 				triggerTransition = iOnAlarmPick;
-			}
-			else if (ResourceTriggerTransition.class.isInstance(tmp)||MessageTriggerTransition.class.isInstance(tmp)){
+			} else if (ResourceTriggerTransition.class.isInstance(tmp)
+					|| MessageTriggerTransition.class.isInstance(tmp))
+			{
 				TOnMessage iOnMessage = iPick.addNewOnMessage();
-				
-				//transition case (1.transition)			
-				if(TPick.class.isInstance(tmp.getBpelCode())){
-					TPick iPickSub = iOnMessage.addNewPick();
-					iPickSub.set(tmp.getBpelCode());			
-				}
-				else if(TSequence.class.isInstance(tmp.getBpelCode())){
-					TSequence iSeq = iOnMessage.addNewSequence();
-					iSeq.set(tmp.getBpelCode());
-				}
-				else if(TIf.class.isInstance(tmp.getBpelCode())){
-					TIf iIf = iOnMessage.addNewIf();
-					iIf.set(tmp.getBpelCode());
-				}
-				else if(TAssign.class.isInstance(tmp.getBpelCode())){
-					TAssign iAss = iOnMessage.addNewAssign();
-					iAss.set(tmp.getBpelCode());			
-				}
-				else if(TFlow.class.isInstance(tmp.getBpelCode())){
-					TFlow iFlowSub = iOnMessage.addNewFlow();
-					iFlowSub.set(tmp.getBpelCode());
-				}		
-				else if(TEmpty.class.isInstance(tmp.getBpelCode())){
-					TEmpty iEmpty = iOnMessage.addNewEmpty();
-					iEmpty.set(tmp.getBpelCode());
-				}
-				else if(TWait.class.isInstance(tmp.getBpelCode())){
-					TWait iWait = iOnMessage.addNewWait();
-					iWait.set(tmp.getBpelCode());
-				}
-				else if(TReceive.class.isInstance(tmp.getBpelCode())){
-					TReceive iReceive = iOnMessage.addNewReceive();
-					iReceive.set(tmp.getBpelCode());
-				}
-				else if(TReply.class.isInstance(tmp.getBpelCode())){
-					TReply iReply = iOnMessage.addNewReply();
-					iReply.set(tmp.getBpelCode());
-				}
-				else if(TInvoke.class.isInstance(tmp.getBpelCode())){
-					TInvoke iInvoke = iOnMessage.addNewInvoke();
-					iInvoke.set(tmp.getBpelCode());
-				}
+
+				// transition case (1.transition)
+				tmp = tmp.get_first_post_element();
+				tmp = tmp.get_first_post_element();
+				this.addToOnMessage(iOnMessage, tmp);
 				triggerTransition = iOnMessage;
-			}				
-			
-			
-			/*//place between the transitions
-			tmp = tmp.get_first_post_element();
-			
-			//3.element
-			tmp = tmp.get_first_post_element();				
-			
-			if(TOnAlarmPick.class.isInstance(triggerTransition)){
-				TOnAlarmPick onAlarmPick = (TOnAlarmPick)triggerTransition;
-				
-				//transitions case (2.transition)
-				if(TAssign.class.isInstance(begin.getBpelCode())){
-					TAssign iAss = onAlarmPick.addNewAssign();
-					iAss.set(begin.getBpelCode());			
-				}		
-				else if(TEmpty.class.isInstance(begin.getBpelCode())){
-					TEmpty iEmpty = onAlarmPick.addNewEmpty();
-					iEmpty.set(begin.getBpelCode());
-				}
-				else if(TWait.class.isInstance(begin.getBpelCode())){
-					TWait iWait = onAlarmPick.addNewWait();
-					iWait.set(begin.getBpelCode());
-				}
-				else if(TReceive.class.isInstance(begin.getBpelCode())){
-					TReceive iReceive = onAlarmPick.addNewReceive();
-					iReceive.set(begin.getBpelCode());
-				}
-				else if(TReply.class.isInstance(begin.getBpelCode())){
-					TReply iReply = onAlarmPick.addNewReply();
-					iReply.set(begin.getBpelCode());
-				}
-				else if(TInvoke.class.isInstance(begin.getBpelCode())){
-					TInvoke iInvoke = onAlarmPick.addNewInvoke();
-					iInvoke.set(begin.getBpelCode());
-				}
 			}
-			else if(TOnMessage.class.isInstance(triggerTransition)){
-				TOnMessage onMessage = (TOnMessage)triggerTransition;
-				//transition case (2.transition)
-				if(TAssign.class.isInstance(begin.getBpelCode())){
-					TAssign iAss = onMessage.addNewAssign();
-					iAss.set(begin.getBpelCode());			
-				}		
-				else if(TEmpty.class.isInstance(begin.getBpelCode())){
-					TEmpty iEmpty = onMessage.addNewEmpty();
-					iEmpty.set(begin.getBpelCode());
-				}
-				else if(TWait.class.isInstance(begin.getBpelCode())){
-					TWait iWait = onMessage.addNewWait();
-					iWait.set(begin.getBpelCode());
-				}
-				else if(TReceive.class.isInstance(begin.getBpelCode())){
-					TReceive iReceive = onMessage.addNewReceive();
-					iReceive.set(begin.getBpelCode());
-				}
-				else if(TReply.class.isInstance(begin.getBpelCode())){
-					TReply iReply = onMessage.addNewReply();
-					iReply.set(begin.getBpelCode());
-				}
-				else if(TInvoke.class.isInstance(begin.getBpelCode())){
-					TInvoke iInvoke = onMessage.addNewInvoke();
-					iInvoke.set(begin.getBpelCode());
-				}
-			}*/			
 		}
 		this.pick = iPick;
 		return pick;
+	}
+
+	private void addToOnAlarm(TOnAlarmPick iOnAlarmPick, AbstractElement tmp)
+	{
+		if (TPick.class.isInstance(tmp.getBpelCode()))
+		{
+			TPick iPickSub = iOnAlarmPick.addNewPick();
+			iPickSub.set(tmp.getBpelCode());
+		} else if (TSequence.class.isInstance(tmp.getBpelCode()))
+		{
+			TSequence iSeq = iOnAlarmPick.addNewSequence();
+			iSeq.set(tmp.getBpelCode());
+		} else if (TIf.class.isInstance(tmp.getBpelCode()))
+		{
+			TIf iIf = iOnAlarmPick.addNewIf();
+			iIf.set(tmp.getBpelCode());
+		} else if (TAssign.class.isInstance(tmp.getBpelCode()))
+		{
+			TAssign iAss = iOnAlarmPick.addNewAssign();
+			iAss.set(tmp.getBpelCode());
+		} else if (TFlow.class.isInstance(tmp.getBpelCode()))
+		{
+			TFlow iFlowSub = iOnAlarmPick.addNewFlow();
+			iFlowSub.set(tmp.getBpelCode());
+		} else if (TEmpty.class.isInstance(tmp.getBpelCode()))
+		{
+			TEmpty iEmpty = iOnAlarmPick.addNewEmpty();
+			iEmpty.set(tmp.getBpelCode());
+		} else if (TWait.class.isInstance(tmp.getBpelCode()))
+		{
+			TWait iWait = iOnAlarmPick.addNewWait();
+			iWait.set(tmp.getBpelCode());
+		} else if (TReceive.class.isInstance(tmp.getBpelCode()))
+		{
+			TReceive iReceive = iOnAlarmPick.addNewReceive();
+			iReceive.set(tmp.getBpelCode());
+		} else if (TReply.class.isInstance(tmp.getBpelCode()))
+		{
+			TReply iReply = iOnAlarmPick.addNewReply();
+			iReply.set(tmp.getBpelCode());
+		} else if (TInvoke.class.isInstance(tmp.getBpelCode()))
+		{
+			TInvoke iInvoke = iOnAlarmPick.addNewInvoke();
+			iInvoke.set(tmp.getBpelCode());
+		}
+	}
+
+	private void addToOnMessage(TOnMessage iOnMessage, AbstractElement tmp)
+	{
+		if (TPick.class.isInstance(tmp.getBpelCode()))
+		{
+			TPick iPickSub = iOnMessage.addNewPick();
+			iPickSub.set(tmp.getBpelCode());
+		} else if (TSequence.class.isInstance(tmp.getBpelCode()))
+		{
+			TSequence iSeq = iOnMessage.addNewSequence();
+			iSeq.set(tmp.getBpelCode());
+		} else if (TIf.class.isInstance(tmp.getBpelCode()))
+		{
+			TIf iIf = iOnMessage.addNewIf();
+			iIf.set(tmp.getBpelCode());
+		} else if (TAssign.class.isInstance(tmp.getBpelCode()))
+		{
+			TAssign iAss = iOnMessage.addNewAssign();
+			iAss.set(tmp.getBpelCode());
+		} else if (TFlow.class.isInstance(tmp.getBpelCode()))
+		{
+			TFlow iFlowSub = iOnMessage.addNewFlow();
+			iFlowSub.set(tmp.getBpelCode());
+		} else if (TEmpty.class.isInstance(tmp.getBpelCode()))
+		{
+			TEmpty iEmpty = iOnMessage.addNewEmpty();
+			iEmpty.set(tmp.getBpelCode());
+		} else if (TWait.class.isInstance(tmp.getBpelCode()))
+		{
+			TWait iWait = iOnMessage.addNewWait();
+			iWait.set(tmp.getBpelCode());
+		} else if (TReceive.class.isInstance(tmp.getBpelCode()))
+		{
+			TReceive iReceive = iOnMessage.addNewReceive();
+			iReceive.set(tmp.getBpelCode());
+		} else if (TReply.class.isInstance(tmp.getBpelCode()))
+		{
+			TReply iReply = iOnMessage.addNewReply();
+			iReply.set(tmp.getBpelCode());
+		} else if (TInvoke.class.isInstance(tmp.getBpelCode()))
+		{
+			TInvoke iInvoke = iOnMessage.addNewInvoke();
+			iInvoke.set(tmp.getBpelCode());
+		}
 	}
 }
