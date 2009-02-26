417e639d9fce76e171f217394bb3245152584a90
mnoeltner <mnoeltner>
2009-02-26 21:06:34 +0000

    Did some code cleaning and added comments.

--- WoPeD-CORE/src/org/woped/core/analysis/FlowArc.java
+++ WoPeD-CORE/src/org/woped/core/analysis/FlowArc.java
@@ -1,10 +1,34 @@
 package org.woped.core.analysis;
 
+/**
+ * @authors Bernhard von Hasseln, Matthias Mruzek and Markus Noeltner <br>
+ * 
+ * This class connects flow nodes in a low level net. In getMaxFlow() in the
+ * class LowLevelNet the flow on each arc is noted in its attribute "flow".
+ * <br>
+ */
+
 public class FlowArc
 {
+	//! Maximum capacity of an arc
 	private final int capacity = 1;
+	
+	//! In getMaxFlow() (class LowLevelNet) the current flow is noted
+	//! in this attribute.
 	private int flow = 0;
+	
+	//! Source of the arc
+	private FlowNode source;
+	
+	//! Target of the arc
+	private FlowNode target;
 
+	//! Constructor
+	FlowArc(FlowNode source, FlowNode target){
+		this.source = source;
+		this.target = target;
+	}
+	
 	public int getFlow() {
 		return flow;
 	}
@@ -19,14 +43,6 @@ public class FlowArc
 	
 	public void subFlow(int flow) {
 		this.flow -= flow;
-	}
-
-	private FlowNode source;
-	private FlowNode target;
-	
-	FlowArc(FlowNode source, FlowNode target){
-		this.source = source;
-		this.target = target;
 	}
 	
 	FlowNode getSource(){
