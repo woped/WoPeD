b446c511ac385a293a4cf53ee2cb7f0097f4b561
coder37 <coder37>
2010-03-01 22:07:08 +0000

    method getNonLiveTransitionsIterator() runtime optimization. if no dead transitions found in the net and the marking net is strongly connected, the petri net is live.

--- WoPeD-QualAnalysis/src/org/woped/qualanalysis/soundness/SoundnessCheckImplement.java
+++ WoPeD-QualAnalysis/src/org/woped/qualanalysis/soundness/SoundnessCheckImplement.java
@@ -16,78 +16,79 @@ import org.woped.qualanalysis.soundness.marking.MarkingNet;
 
 public class SoundnessCheckImplement implements ISoundnessCheck {
 
-	private IEditor editor;
-	private MarkingNet markingNetWithoutTStar;
-	private MarkingNet markingNetWithTStar;
+    private IEditor editor;
+    private MarkingNet markingNetWithoutTStar;
+    private MarkingNet markingNetWithTStar;
 
-	public SoundnessCheckImplement(IEditor editor) {
-		this.editor = editor;
-		markingNetWithoutTStar = BuilderFactory.createMarkingNet(BuilderFactory
-				.createLowLevelPetriNetWithoutTStarBuilder(editor).getLowLevelPetriNet());
-		markingNetWithTStar = BuilderFactory.createMarkingNet(BuilderFactory.createLowLevelPetriNetWithTStarBuilder(
-				editor).getLowLevelPetriNet());
-	}
+    public SoundnessCheckImplement(IEditor editor) {
+        this.editor = editor;
+        markingNetWithoutTStar = BuilderFactory.createMarkingNet(BuilderFactory
+                .createLowLevelPetriNetWithoutTStarBuilder(editor).getLowLevelPetriNet());
+        markingNetWithTStar = BuilderFactory.createMarkingNet(BuilderFactory.createLowLevelPetriNetWithTStarBuilder(
+                editor).getLowLevelPetriNet());
+    }
 
-	public MarkingNet getMarkingNet() {
-		return this.markingNetWithoutTStar;
-	}
+    public MarkingNet getMarkingNet() {
+        return this.markingNetWithoutTStar;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.woped.qualanalysis.ISoundnessCheck#getNonLiveTransitionsIterator()
-	 */
-	@Override
-	public Iterator<AbstractElementModel> getNonLiveTransitionsIterator() {
-		Set<AbstractElementModel> nonLiveTransitions = new HashSet<AbstractElementModel>();
-		for (TransitionNode transition : AlgorithmFactory.createNonLiveTranstionTest(markingNetWithTStar)
-				.getNonLiveTransitions()) {
-			nonLiveTransitions.add(getAEM(transition));
-		}
-		// remove t* if existing
-		nonLiveTransitions.remove(null);
-		return nonLiveTransitions.iterator();
-	}
+    /*
+     * (non-Javadoc)
+     * @see org.woped.qualanalysis.ISoundnessCheck#getNonLiveTransitionsIterator() runtime optimization. if no dead transitions found in the net and the marking
+     * net is strongly connected, the petri net is live.
+     */
+    @Override
+    public Iterator<AbstractElementModel> getNonLiveTransitionsIterator() {
+        Set<AbstractElementModel> nonLiveTransitions = new HashSet<AbstractElementModel>();
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.woped.qualanalysis.ISoundnessCheck#getDeadTransitionsIterator()
-	 */
-	@Override
-	public Iterator<AbstractElementModel> getDeadTransitionsIterator() {
-		Set<AbstractElementModel> deadTransitions = new HashSet<AbstractElementModel>();
-		for (TransitionNode transition : AlgorithmFactory.createDeadTransitionTest(markingNetWithoutTStar)
-				.getDeadTransitions()) {
-			deadTransitions.add(getAEM(transition));
-		}
-		// remove t* if existing
-		deadTransitions.remove(null);
-		return deadTransitions.iterator();
-	}
+        if (getDeadTransitionsIterator().hasNext()
+                || !AlgorithmFactory.createStronglyConnectedComponentTest(markingNetWithTStar).isStronglyConnected()) {
+            for (TransitionNode transition : AlgorithmFactory.createNonLiveTranstionTest(markingNetWithTStar)
+                    .getNonLiveTransitions()) {
+                nonLiveTransitions.add(getAEM(transition));
+            }
+        }
+        // remove t* if existing
+        nonLiveTransitions.remove(null);
+        return nonLiveTransitions.iterator();
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.woped.qualanalysis.ISoundnessCheck#getUnboundedPlacesIterator()
-	 */
-	@Override
-	public Iterator<AbstractElementModel> getUnboundedPlacesIterator() {
-		Set<AbstractElementModel> unboundedPlaces = new HashSet<AbstractElementModel>();
-		for (PlaceNode place : AlgorithmFactory.createUnboundedPlacesTest(markingNetWithTStar).getUnboundedPlaces()) {
-			unboundedPlaces.add(getAEM(place));
-		}
-		return unboundedPlaces.iterator();
-	}
+    /*
+     * (non-Javadoc)
+     * @see org.woped.qualanalysis.ISoundnessCheck#getDeadTransitionsIterator()
+     */
+    @Override
+    public Iterator<AbstractElementModel> getDeadTransitionsIterator() {
+        Set<AbstractElementModel> deadTransitions = new HashSet<AbstractElementModel>();
+        for (TransitionNode transition : AlgorithmFactory.createDeadTransitionTest(markingNetWithoutTStar)
+                .getDeadTransitions()) {
+            deadTransitions.add(getAEM(transition));
+        }
+        // remove t* if existing
+        deadTransitions.remove(null);
+        return deadTransitions.iterator();
+    }
 
-	/**
-	 * 
-	 * @param node
-	 *            the AbstractNode to get the referring AbstractElementModel from
-	 * @return the referred AbstractElementModel
-	 */
-	private AbstractElementModel getAEM(AbstractNode node) {
-		return editor.getModelProcessor().getElementContainer().getElementById(node.getOriginId());
-	}
+    /*
+     * (non-Javadoc)
+     * @see org.woped.qualanalysis.ISoundnessCheck#getUnboundedPlacesIterator()
+     */
+    @Override
+    public Iterator<AbstractElementModel> getUnboundedPlacesIterator() {
+        Set<AbstractElementModel> unboundedPlaces = new HashSet<AbstractElementModel>();
+        for (PlaceNode place : AlgorithmFactory.createUnboundedPlacesTest(markingNetWithTStar).getUnboundedPlaces()) {
+            unboundedPlaces.add(getAEM(place));
+        }
+        return unboundedPlaces.iterator();
+    }
+
+    /**
+     * 
+     * @param node the AbstractNode to get the referring AbstractElementModel from
+     * @return the referred AbstractElementModel
+     */
+    private AbstractElementModel getAEM(AbstractNode node) {
+        return editor.getModelProcessor().getElementContainer().getElementById(node.getOriginId());
+    }
 
 }
