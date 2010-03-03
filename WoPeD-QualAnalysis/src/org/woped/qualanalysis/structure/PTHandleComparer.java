9df4533b54394b1590d8b24005d23722db0480c6
coder37 <coder37>
2010-03-03 22:07:33 +0000

    PT and TP HandleComparer changed. compares now the name of the model and only the id if the name ist null

--- WoPeD-QualAnalysis/src/org/woped/qualanalysis/structure/PTHandleComparer.java
+++ WoPeD-QualAnalysis/src/org/woped/qualanalysis/structure/PTHandleComparer.java
@@ -5,12 +5,21 @@ import java.util.Comparator;
 import org.woped.core.model.AbstractElementModel;
 import org.woped.core.model.petrinet.PlaceModel;
 
+/**
+ * compares to abstract element models. Places are always before transitions.
+ * 
+ * @author Patrick Spies, Patrick Kirchgaessner, Joern Liebau, Enrico Moeller, Sebastian Fuss
+ * 
+ */
 public class PTHandleComparer implements Comparator<AbstractElementModel> {
 
     @Override
     public int compare(AbstractElementModel o1, AbstractElementModel o2) {
 
         if (o1.getClass() == o2.getClass()) {
+            if (o1.getNameValue() != null) {
+                return o1.getNameValue().compareTo(o2.getNameValue());
+            }
             return o1.getId().compareTo(o2.getId());
         }
 
