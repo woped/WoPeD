a507cb087a56f553e1d90d2a7c46eda85ec65b2b
coder37 <coder37>
2010-06-08 07:40:42 +0000

    two methods added to transform set of AbstractNodes to set of AbstractElementModelNodes

--- WoPeD-QualAnalysis/src/org/woped/qualanalysis/soundness/WorkflowCheckImplement.java
+++ WoPeD-QualAnalysis/src/org/woped/qualanalysis/soundness/WorkflowCheckImplement.java
@@ -11,8 +11,6 @@ import org.woped.qualanalysis.soundness.algorithms.basedonlowlevelpetrinet.sourc
 import org.woped.qualanalysis.soundness.builder.BuilderFactory;
 import org.woped.qualanalysis.soundness.datamodel.AbstractNode;
 import org.woped.qualanalysis.soundness.datamodel.LowLevelPetriNet;
-import org.woped.qualanalysis.soundness.datamodel.PlaceNode;
-import org.woped.qualanalysis.soundness.datamodel.TransitionNode;
 import org.woped.qualanalysis.structure.StructuralAnalysis;
 
 /**
@@ -23,173 +21,134 @@ import org.woped.qualanalysis.structure.StructuralAnalysis;
  */
 public class WorkflowCheckImplement implements IWorkflowCheck {
 
-	private IEditor editor = null;
-	private ISourceSinkTest sourceSinkTest = null;
-	private LowLevelPetriNet lolNetWithTStar = null;
-	private LowLevelPetriNet lolNetWithoutTStar = null;
+    private IEditor editor = null;
+    private ISourceSinkTest sourceSinkTest = null;
+    private LowLevelPetriNet lolNetWithTStar = null;
+    private LowLevelPetriNet lolNetWithoutTStar = null;
 
-	private Set<AbstractElementModel> sourcePlaces = null;
-	private Set<AbstractElementModel> sinkPlaces = null;
-	private Set<AbstractElementModel> sourceTransitions = null;
-	private Set<AbstractElementModel> sinkTransitions = null;
-	private Set<Set<AbstractNode>> uccs = null;
-	private Set<Set<AbstractElementModel>> sccs = null;
-	private Set<Set<AbstractElementModel>> ccs = null;
+    private Set<AbstractElementModel> sourcePlaces = null;
+    private Set<AbstractElementModel> sinkPlaces = null;
+    private Set<AbstractElementModel> sourceTransitions = null;
+    private Set<AbstractElementModel> sinkTransitions = null;
+    private Set<Set<AbstractElementModel>> stronglyConnectedComponents = null;
+    private Set<Set<AbstractElementModel>> connectedComponents = null;
 
-	public WorkflowCheckImplement(IEditor editor) {
-		this.editor = editor;
-	}
-	
-	/**
-	 * 
-	 * @return the LowLevelPetriNet without t* (if not existing it will be instantiated)
-	 */
-	public LowLevelPetriNet getLolNetWithoutTStar(){
-		if(lolNetWithoutTStar == null){
-			lolNetWithoutTStar = BuilderFactory.createLowLevelPetriNetWithoutTStarBuilder(editor).getLowLevelPetriNet();
-		}
-		return lolNetWithoutTStar;
-	}
-	
-	/**
-	 * 
-	 * @return the LowLevelPetriNet without t* (if not existing it will be instantiated)
-	 */
-	public LowLevelPetriNet getLolNetWithTStar(){
-		if(lolNetWithTStar == null){
-			lolNetWithTStar = BuilderFactory.createLowLevelPetriNetWithTStarBuilder(editor).getLowLevelPetriNet();
-		}
-		return lolNetWithTStar;
-	}
-	
-	/**
-	 * 
-	 * @return the SourceSinkTest (if not existing it will be instantiated)
-	 */
-	public ISourceSinkTest getSourceSinkTest(){
-		if(sourceSinkTest == null){
-			sourceSinkTest = AlgorithmFactory.createSourceSinkTest(getLolNetWithoutTStar());
-		}
-		return sourceSinkTest;
-	}
+    public WorkflowCheckImplement(IEditor editor) {
+        this.editor = editor;
+    }
 
-	@Override
-	public Set<AbstractElementModel> getSourcePlaces() {
-		if (sourcePlaces == null) {
-			sourcePlaces = new HashSet<AbstractElementModel>();
-			for (PlaceNode place : getSourceSinkTest().getSourcePlaces()) {
-				sourcePlaces.add(getAEM(place));
-			}
-		}
-		return sourcePlaces;
-	}
+    /**
+     * 
+     * @return the LowLevelPetriNet without t* (if not existing it will be instantiated)
+     */
+    public LowLevelPetriNet getLolNetWithoutTStar() {
+        if (lolNetWithoutTStar == null) {
+            lolNetWithoutTStar = BuilderFactory.createLowLevelPetriNetWithoutTStarBuilder(editor).getLowLevelPetriNet();
+        }
+        return lolNetWithoutTStar;
+    }
 
-	@Override
-	public Set<AbstractElementModel> getSinkPlaces() {
-		if (sinkPlaces == null) {
-			sinkPlaces = new HashSet<AbstractElementModel>();
-			for (PlaceNode place : getSourceSinkTest().getSinkPlaces()) {
-				sinkPlaces.add(getAEM(place));
-			}
-		}
-		return sinkPlaces;
-	}
+    /**
+     * 
+     * @return the LowLevelPetriNet without t* (if not existing it will be instantiated)
+     */
+    public LowLevelPetriNet getLolNetWithTStar() {
+        if (lolNetWithTStar == null) {
+            lolNetWithTStar = BuilderFactory.createLowLevelPetriNetWithTStarBuilder(editor).getLowLevelPetriNet();
+        }
+        return lolNetWithTStar;
+    }
 
-	@Override
-	public Set<AbstractElementModel> getSourceTransitions() {
-		if (sourceTransitions == null) {
-			sourceTransitions = new HashSet<AbstractElementModel>();
-			for (TransitionNode transition : getSourceSinkTest().getSourceTransitions()) {
-				sourceTransitions.add(getAEM(transition));
-			}
-		}
-		return sourceTransitions;
-	}
+    /**
+     * 
+     * @return the SourceSinkTest (if not existing it will be instantiated)
+     */
+    public ISourceSinkTest getSourceSinkTest() {
+        if (sourceSinkTest == null) {
+            sourceSinkTest = AlgorithmFactory.createSourceSinkTest(getLolNetWithoutTStar());
+        }
+        return sourceSinkTest;
+    }
 
-	@Override
-	public Set<AbstractElementModel> getSinkTransitions() {
-		if (sinkTransitions == null) {
-			sinkTransitions = new HashSet<AbstractElementModel>();
+    @Override
+    public Set<AbstractElementModel> getSourcePlaces() {
+        if (sourcePlaces == null) {
 
-			for (TransitionNode transition : getSourceSinkTest().getSinkTransitions()) {
-				sinkTransitions.add(getAEM(transition));
-			}
-		}
-		return sinkTransitions;
-	}
+            sourcePlaces = translateAbstractNodeSet2AEMSet(getSourceSinkTest().getSourcePlaces());
+        }
+        return sourcePlaces;
+    }
 
-	@Override
-	public Set<AbstractElementModel> getNotConnectedNodes() {
-		if (uccs == null) {
-			uccs = AlgorithmFactory.createCcTest(getLolNetWithoutTStar()).getConnectedComponents();
-		}
+    @Override
+    public Set<AbstractElementModel> getSinkPlaces() {
+        if (sinkPlaces == null) {
 
-		if (uccs.size() > 1) {
-			return new HashSet<AbstractElementModel>(editor.getModelProcessor().getElementContainer().getRootElements());
-		}
+            sinkPlaces = translateAbstractNodeSet2AEMSet(getSourceSinkTest().getSinkPlaces());
 
-		return new HashSet<AbstractElementModel>();
-	}
+        }
+        return sinkPlaces;
+    }
 
-	@Override
-	public Set<AbstractElementModel> getNotStronglyConnectedNodes() {
-		// delegate
-		return new StructuralAnalysis(editor).getNotStronglyConnectedNodes();
-	}
+    @Override
+    public Set<AbstractElementModel> getSourceTransitions() {
+        if (sourceTransitions == null) {
 
-	@Override
-	public Set<Set<AbstractElementModel>> getStronglyConnectedComponents() {
-		if (sccs == null) {
-			sccs = new HashSet<Set<AbstractElementModel>>();
+            sourceTransitions = translateAbstractNodeSet2AEMSet(getSourceSinkTest().getSourceTransitions());
+        }
+        return sourceTransitions;
+    }
 
-			Set<AbstractElementModel> scc;
-			for (Set<AbstractNode> set : AlgorithmFactory.createSccTest(getLolNetWithTStar())
-					.getStronglyConnectedComponents()) {
-				if (set.size() > 0) {
-					scc = new HashSet<AbstractElementModel>();
+    @Override
+    public Set<AbstractElementModel> getSinkTransitions() {
+        if (sinkTransitions == null) {
 
-					for (AbstractNode node : set) {
-						scc.add(getAEM(node));
-					}
-					// remove t*
-					scc.remove(null);
-					if (scc.size() > 0) {
-						sccs.add(scc);
-					}
-				}
-			}
-		}
+            sinkTransitions = translateAbstractNodeSet2AEMSet(getSourceSinkTest().getSinkTransitions());
+        }
+        return sinkTransitions;
+    }
 
-		return sccs;
-	}
+    @Override
+    public Set<AbstractElementModel> getNotConnectedNodes() {
 
-	@Override
-	public Set<Set<AbstractElementModel>> getConnectedComponents() {
-		if (ccs == null) {
-			ccs = new HashSet<Set<AbstractElementModel>>();
-			Set<AbstractElementModel> cc;
-			for (Set<AbstractNode> set : AlgorithmFactory.createCcTest(getLolNetWithoutTStar()).getConnectedComponents()) {
-				if (set.size() > 0) {
-					cc = new HashSet<AbstractElementModel>();
+        if (getConnectedComponents().size() > 1) {
 
-					for (AbstractNode node : set) {
-						cc.add(getAEM(node));
-					}
-					// remove t*
-					cc.remove(null);
-					if (cc.size() > 0) {
-						ccs.add(cc);
-					}
-				}
-			}
-		}
+            return new HashSet<AbstractElementModel>(editor.getModelProcessor().getElementContainer().getRootElements());
+        }
 
-		return ccs;
-	}
+        return new HashSet<AbstractElementModel>();
+    }
 
-	@Override
-	public boolean isWorkflowNet() {
-		if (getSourcePlaces().size() != 1) {
+    @Override
+    public Set<AbstractElementModel> getNotStronglyConnectedNodes() {
+        // delegate
+        return new StructuralAnalysis(editor).getNotStronglyConnectedNodes();
+    }
+
+    @Override
+    public Set<Set<AbstractElementModel>> getStronglyConnectedComponents() {
+        if (stronglyConnectedComponents == null) {
+
+            stronglyConnectedComponents = translateAbstractNodeSetSet2AEMSetSet(AlgorithmFactory.createSccTest(
+                    getLolNetWithTStar()).getStronglyConnectedComponents());
+        }
+
+        return stronglyConnectedComponents;
+    }
+
+    @Override
+    public Set<Set<AbstractElementModel>> getConnectedComponents() {
+        if (connectedComponents == null) {
+
+            connectedComponents = translateAbstractNodeSetSet2AEMSetSet(AlgorithmFactory.createCcTest(
+                    getLolNetWithoutTStar()).getConnectedComponents());
+        }
+
+        return connectedComponents;
+    }
+
+    @Override
+    public boolean isWorkflowNet() {
+        if (getSourcePlaces().size() != 1) {
             return false;
         }
         if (getSinkPlaces().size() != 1) {
@@ -208,16 +167,61 @@ public class WorkflowCheckImplement implements IWorkflowCheck {
             return false;
         }
         return true;
-	}
+    }
 
-	/**
-	 * 
-	 * @param node
-	 *            the AbstractNode to get the referring AbstractElementModel from
-	 * @return the referred AbstractElementModel
-	 */
-	private AbstractElementModel getAEM(AbstractNode node) {
-		return editor.getModelProcessor().getElementContainer().getElementById(node.getOriginId());
-	}
+    /**
+     * 
+     * @param node the AbstractNode to get the referring AbstractElementModel from
+     * @return the referred AbstractElementModel
+     */
+    private AbstractElementModel getAEM(AbstractNode node) {
+        return editor.getModelProcessor().getElementContainer().getElementById(node.getOriginId());
+    }
+
+    /**
+     * set transformation.
+     * 
+     * @author Sebastian Fuﬂ
+     * @param abstractNodeSet set of AbstractNode objects
+     * @return set of AbstractElementModel objects
+     */
+    private Set<AbstractElementModel> translateAbstractNodeSet2AEMSet(Set<? extends AbstractNode> abstractNodeSet) {
+        Set<AbstractElementModel> aemSet = new HashSet<AbstractElementModel>();
+
+        for (AbstractNode node : abstractNodeSet) {
+            aemSet.add(getAEM(node));
+        }
+
+        // remove null value
+        aemSet.remove(null);
+
+        return aemSet;
+    }
+
+    /**
+     * double set transformation.
+     * 
+     * @author Sebastian Fuﬂ
+     * @param abstractNodeSetSet set of an set of AbstractNode objects
+     * @return set of an set of AbstractElementModel-Objects
+     */
+    private Set<Set<AbstractElementModel>> translateAbstractNodeSetSet2AEMSetSet(
+            Set<Set<AbstractNode>> abstractNodeSetSet) {
+
+        Set<Set<AbstractElementModel>> aemSetSet = new HashSet<Set<AbstractElementModel>>();
+        Set<AbstractElementModel> aemSet;
+
+        for (Set<AbstractNode> set : abstractNodeSetSet) {
+            if (set.size() > 0) {
+                aemSet = translateAbstractNodeSet2AEMSet(set);
+
+                if (aemSet != null && aemSet.size() > 0) {
+                    aemSetSet.add(aemSet);
+                }
+            }
+        }
+
+        return aemSetSet;
+    }
 
 }
