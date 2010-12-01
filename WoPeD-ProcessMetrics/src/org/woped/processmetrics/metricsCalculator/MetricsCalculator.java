382ca4f924ae155a11364eee5526cabbdd07a9ab
holgerkraus <holgerkraus>
2010-12-01 14:17:42 +0000

    Working metrics calculation. Furthermore possible calculation and test case for 10 'counts' and 8 'relations'

--- WoPeD-ProcessMetrics/src/org/woped/processmetrics/metricsCalculator/MetricsCalculator.java
+++ WoPeD-ProcessMetrics/src/org/woped/processmetrics/metricsCalculator/MetricsCalculator.java
@@ -2,7 +2,10 @@ package org.woped.processmetrics.metricsCalculator;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
+import org.woped.core.model.AbstractElementModel;
+import org.woped.core.model.ArcModel;
 import org.woped.core.model.ModelElementContainer;
 import org.woped.core.model.petrinet.PetriNetModelElement;
 
@@ -25,7 +28,7 @@ public class MetricsCalculator {
 	}
 	
 	public double calculateT(){
-		return mec.getElementsByType(PetriNetModelElement.SUBP_TYPE).size()+mec.getElementsByType(PetriNetModelElement.TRANS_SIMPLE_TYPE).size()+mec.getElementsByType(PetriNetModelElement.TRANS_OPERATOR_TYPE).size();
+		return getTransitions().size();
 	}
 	
 	public double calculateP(){ 
@@ -37,6 +40,24 @@ public class MetricsCalculator {
 	}
 	
 	public double calculateSeqN(){
+		return calculateSequence(0);
+	}
+	
+	public double calculateSeqT(){
+		return calculateSequence(1);
+	}
+	
+	public double calculateSeqP(){
+		return calculateSequence(2);
+	}
+	
+	/**
+	 * 
+	 * @param type
+	 * 	0 = all, 1 = transitions, 2 = places
+	 * @return
+	 */
+	private double calculateSequence(int type){
 		HashMap<String, Integer> inboundLines = new HashMap<String, Integer>();
 		HashMap<String, Integer> outboundLines = new HashMap<String, Integer>();
 		Map<String, Map<String,Object>> origMap = mec.getIdMap();
@@ -47,17 +68,41 @@ public class MetricsCalculator {
 			outboundLines.put(key, origMap.get(key).size()-1);
 			// Adds itself as a parent reference for its children
 			for(String subkey:origMap.get(key).keySet()){
-				if(inboundLines.containsKey(subkey))
-					inboundLines.put(subkey, inboundLines.get(subkey)+1);
+				ArcModel arc = mec.getArcMap().get(subkey);
+				if(arc == null) continue;
+				String childKey = arc.getTargetId();
+				
+				if(inboundLines.containsKey(childKey))
+					inboundLines.put(childKey, inboundLines.get(childKey)+1);
 				else
-					inboundLines.put(subkey, 1);
+					inboundLines.put(childKey, 1);
 			}
 		}
-		for(String key:origMap.keySet()){
-			if(outboundLines.get(key)<2 && inboundLines.get(key)<2)
+		
+		Set<String> map;
+		if(type == 1) map = getTransitions().keySet();
+		else if (type == 2) map = mec.getElementsByType(PetriNetModelElement.PLACE_TYPE).keySet();
+		else map = origMap.keySet();
+		for(String key:map)
+			if(outboundLines.get(key)<2 && (!inboundLines.containsKey(key) || inboundLines.get(key)<2) )
 				seqn++;
-		}
+
 		return seqn;
+	}
+	
+	private Map<String,AbstractElementModel> getTransitions(){
+		Map<String, AbstractElementModel> transitions = new HashMap<String, AbstractElementModel>();
+		Map<String, AbstractElementModel> partialTransitions;
+		partialTransitions = mec.getElementsByType(PetriNetModelElement.SUBP_TYPE);
+		for(String key:partialTransitions.keySet())
+			transitions.put(key,partialTransitions.get(key));
+		partialTransitions = mec.getElementsByType(PetriNetModelElement.TRANS_SIMPLE_TYPE);
+		for(String key:partialTransitions.keySet())
+			transitions.put(key,partialTransitions.get(key));
+		partialTransitions = mec.getElementsByType(PetriNetModelElement.TRANS_OPERATOR_TYPE);
+		for(String key:partialTransitions.keySet())
+			transitions.put(key,partialTransitions.get(key));
+		return transitions;
 	}
 	
 	public double calculate(String formula){
@@ -73,13 +118,42 @@ public class MetricsCalculator {
          	return this.calculateT();
          }else if(token.equalsIgnoreCase("A")){
          	return this.calculateA();
+         }else if(token.equalsIgnoreCase("SeqN")){
+          	return this.calculateSeqN();
+         }else if(token.equalsIgnoreCase("SeqP")){
+           	return this.calculateSeqP();
+         }else if(token.equalsIgnoreCase("SeqT")){
+          	return this.calculateSeqT();
+         }else if(token.equalsIgnoreCase("RN")){
+           	return this.calculate("N - SeqN");
+         }else if(token.equalsIgnoreCase("RP")){
+            	return this.calculate("P - SeqP");
+         }else if(token.equalsIgnoreCase("RT")){
+            	return this.calculate("T - SeqT");
+          	
+          	
          }else if(token.equalsIgnoreCase("D")){
-          	return this.calculate("A / (N * (N-1))");
+          	return this.calculate("A / (N * (N - 1))");
+         }else if(token.equalsIgnoreCase("CNC")){
+           	return this.calculate("(A + 1) / N");
+         }else if(token.equalsIgnoreCase("DeN")){
+            	return this.calculate("A / N");
+         }else if(token.equalsIgnoreCase("DeP")){
+         	return this.calculate("A / P");
+         }else if(token.equalsIgnoreCase("DeT")){
+         	return this.calculate("A / T");
+         }else if(token.equalsIgnoreCase("Seq")){
+          	return this.calculate("SeqN / N");
+         }else if(token.equalsIgnoreCase("RR")){
+           	return this.calculate("RN / N");
+         }else if(token.equalsIgnoreCase("CH")){
+            	return this.calculate("RT / T * log2( RT / T ) + RP / P * log2( RP / P)");
          }else{
-        	 //TODO: Recursiv call of the formula
+        	 //TODO: Recursive call of the formula
         	 return 0;
          }
 		
 	     
 	}
+
 }
---
+++ WoPeD-ProcessMetrics/src/org/woped/processmetrics/metricsCalculator/MetricsUIRequestHandler.java
@@ -0,0 +1,32 @@
+package org.woped.processmetrics.metricsCalculator;
+
+import org.woped.core.model.ModelElementContainer;
+
+public class MetricsUIRequestHandler {
+
+	public void showInitialData(ModelElementContainer mec){
+		MetricsCalculator mc = new MetricsCalculator(mec);
+		System.out.println("=== Start of variables");
+		System.out.println("Nodes(N): "+mc.calculate("N"));
+		System.out.println("Places(P): "+mc.calculate("P"));
+		System.out.println("Transitions(T): "+mc.calculate("T"));
+		System.out.println("Arcs(A): "+mc.calculate("A"));
+		System.out.println("SequenceNodes(SeqN): "+mc.calculate("SeqN"));
+		System.out.println("SequencePlaces(SeqP): "+mc.calculate("SeqP"));
+		System.out.println("SequenceTransitions(SeqT): "+mc.calculate("SeqT"));
+		System.out.println("RoutingNodes(RN): "+mc.calculate("RN"));
+		System.out.println("RoutingPlaces(RP): "+mc.calculate("RP"));
+		System.out.println("RoutingTransitions(RT): "+mc.calculate("RT"));
+		System.out.println("=== Start of calculated values");
+		System.out.println("Density(D): "+mc.calculate("D"));
+		System.out.println("CoefficientOfConnectivity(CNC): "+mc.calculate("CNC"));
+		System.out.println("AvDegreeNodes(DeN): "+mc.calculate("DeN"));
+		System.out.println("AvDegreePlaces(DeP): "+mc.calculate("DeP"));
+		System.out.println("AvDegreeTransitions(DeT): "+mc.calculate("DeT"));
+		System.out.println("Sequentiality(Seq): "+mc.calculate("Seq"));
+		System.out.println("RoutingRatio(RR): "+mc.calculate("RR"));
+		System.out.println("Heterogenity(CH): "+mc.calculate("CH"));
+	}
+	
+	
+}
