7e1c73bceb22cbf4a93b521f73669498645330ae
martin39 <martin39>
2012-05-25 11:23:43 +0000

    *** empty log message ***

--- WoPeD-QualAnalysis/src/org/woped/qualanalysis/paraphrasing/controller/CurrentNetPnml.java
+++ WoPeD-QualAnalysis/src/org/woped/qualanalysis/paraphrasing/controller/CurrentNetPnml.java
@@ -1,9 +1,15 @@
 package org.woped.qualanalysis.paraphrasing.controller;
 
+import java.awt.geom.Point2D;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
+
 import org.apache.xmlbeans.XmlOptions;
+import org.woped.core.config.ConfigurationManager;
 import org.woped.core.controller.IEditor;
 import org.woped.core.model.ArcModel;
 import org.woped.core.model.ModelElementContainer;
@@ -15,12 +21,15 @@ import org.woped.core.model.petrinet.PlaceModel;
 import org.woped.core.model.petrinet.TransitionModel;
 import org.woped.core.utilities.LoggerManager;
 import org.woped.pnml.ArcNameType;
+import org.woped.pnml.ArcToolspecificType;
 import org.woped.pnml.ArcType;
 import org.woped.pnml.NetType;
 import org.woped.pnml.NodeNameType;
 import org.woped.pnml.PlaceType;
 import org.woped.pnml.PnmlDocument;
 import org.woped.pnml.PnmlType;
+import org.woped.pnml.PositionType;
+import org.woped.pnml.ToolspecificType;
 import org.woped.pnml.TransitionType;
 import org.woped.qualanalysis.paraphrasing.Constants;
 
@@ -72,6 +81,8 @@ public class CurrentNetPnml {
             
             this.pnmlString = baos.toString();
             
+//            System.out.println(this.pnmlString);
+            
         }catch (IOException e)
         {
             LoggerManager.error(Constants.PARAPHRASING_LOGGER, "Could not write to the Stream. " + e.getMessage());
@@ -87,36 +98,121 @@ public class CurrentNetPnml {
             AbstractPetriNetElementModel currentModel = (AbstractPetriNetElementModel) root2Iter.next();
             
             
-            /* ##### PLACES ##### */
+            	/* ##### PLACES ##### */
             if (currentModel.getType() == AbstractPetriNetElementModel.PLACE_TYPE)
             {
                 initPlace(iNet.addNewPlace(), (PlaceModel) currentModel);
             } else if (currentModel.getType() == AbstractPetriNetElementModel.TRANS_SIMPLE_TYPE)
-            /* ##### NORMAL TRANSITION ##### */
+            	/* ##### NORMAL TRANSITION ##### */
             {
                 initTransition(iNet.addNewTransition(), (TransitionModel) currentModel, null);
 
-            }else if (currentModel.getType() == AbstractPetriNetElementModel.SUBP_TYPE || 
-            		currentModel.getType() == AbstractPetriNetElementModel.TRANS_OPERATOR_TYPE)
+            }else if (currentModel.getType() == AbstractPetriNetElementModel.SUBP_TYPE)
             	/* ##### SUB TRANSITION ##### */
             {
                 initTransition(iNet.addNewTransition(), (TransitionModel) currentModel, null);
+            } else if (currentModel.getType() == AbstractPetriNetElementModel.TRANS_OPERATOR_TYPE)
+            {
+            	OperatorTransitionModel operatorModel = (OperatorTransitionModel) currentModel;
+                Iterator<AbstractPetriNetElementModel> simpleTransIter = operatorModel.getSimpleTransContainer().getElementsByType(AbstractPetriNetElementModel.TRANS_SIMPLE_TYPE).values().iterator();
+                while (simpleTransIter.hasNext())
+                {
+                    AbstractPetriNetElementModel simpleTransModel = (AbstractPetriNetElementModel) simpleTransIter.next();
+                    if (simpleTransModel != null // Sometimes the iterator
+                            // returns null...
+                            && operatorModel.getSimpleTransContainer().getElementById(simpleTransModel.getId()).getType() == AbstractPetriNetElementModel.TRANS_SIMPLE_TYPE)
+                    {
+                        initTransition(iNet.addNewTransition(), (TransitionModel) operatorModel.getSimpleTransContainer().getElementById(simpleTransModel.getId()), operatorModel);
+                    }
+
+                }
+//                if (operatorModel.getCenterPlace() != null)
+//                {
+//                    PlaceType iCenterPlace = initPlace(iNet.addNewPlace(), operatorModel.getCenterPlace());
+//                    initToolspecific(iCenterPlace.addNewToolspecific(), operatorModel.getCenterPlace(), operatorModel.getId(), operatorModel.getOperatorType());
+//                }
             }
          
             
         }
         /* ##### ARCS ##### */
         
-        Iterator<String> arcIter = elementContainer.getArcMap().keySet().iterator();
+//        Iterator<String> arcIter = elementContainer.getArcMap().keySet().iterator();
+//        
+//        while (arcIter.hasNext())
+//        {
+//            ArcModel currentArc = elementContainer.getArcById(arcIter.next());
+//
+//    		initArc(iNet.addNewArc(), currentArc);
+//
+//        }
         
+        Set<AbstractPetriNetElementModel> connectedTransitions = new HashSet<AbstractPetriNetElementModel>();  
+        Iterator<String> arcIter = elementContainer.getArcMap().keySet().iterator();
         while (arcIter.hasNext())
         {
             ArcModel currentArc = elementContainer.getArcById(arcIter.next());
-
-    		initArc(iNet.addNewArc(), currentArc);
-
+            AbstractPetriNetElementModel currentTargetModel = (AbstractPetriNetElementModel) elementContainer.getElementById(currentArc.getTargetId());
+            AbstractPetriNetElementModel currentSourceModel = (AbstractPetriNetElementModel) elementContainer.getElementById(currentArc.getSourceId());
+            // Remember either source or target if it is a transition
+            // Please note that one special condition of petri nets is that
+            // a transition is never directly connected to another transition
+            // so either source or target may be a transition, never both
+            if (currentTargetModel.getType() == AbstractPetriNetElementModel.TRANS_OPERATOR_TYPE)
+            	connectedTransitions.add(currentTargetModel);
+            else if (currentSourceModel.getType() == AbstractPetriNetElementModel.TRANS_OPERATOR_TYPE)
+            	connectedTransitions.add(currentSourceModel);
+            else
+            {
+            	// The current arc is not connected to any transition
+            	// We do not need to take care of any inner arcs
+            	// and instead store the currentArc itself
+                initArc(iNet.addNewArc(), currentArc, null);
+            }
+//            System.out.println(currentArc.getId());
+            
         }
-        
+    	// A transition can be a very complex construct consisting
+    	// of a lot more than just one primitive petri-net transition (e.g.
+    	// XOR Split, XOR Join, ...
+    	// When dumping the PNML structure we must create primitive petri-net
+    	// objects for applications that cannot read our tool specific
+    	// complex transitions
+    	// This is why all transitions store a map of primitive transitions
+    	// with (ID, Object-Reference) entries.
+        // For all transitions connected to at least one arc we will
+        // dump the internal arcs now instead of the (previously ignored) visible arcs
+        Iterator<AbstractPetriNetElementModel> currentTransition = connectedTransitions.iterator();
+        while (currentTransition.hasNext())
+        {
+        	OperatorTransitionModel currentConnectedModel = (OperatorTransitionModel)currentTransition.next();
+        	Iterator<String> innerArcIter = currentConnectedModel.getSimpleTransContainer().getArcMap().keySet().iterator();
+        	while (innerArcIter.hasNext())
+        	{
+           		// Dump all inner arcs of connected transitions
+        		ArcModel currentInnerArc = (ArcModel) currentConnectedModel.getSimpleTransContainer().getArcMap().get(innerArcIter.next());
+        		// Find outer arc corresponding to inner arc
+        		// (carries graphics information)
+        		ArcModel currentOuterArc = null;
+        		if (elementContainer.getElementById(currentInnerArc.getSourceId())!=null)
+        		{
+        			currentOuterArc = elementContainer.findArc(currentInnerArc.getSourceId(),
+        					currentConnectedModel.getId()); 
+        		}
+        		if (elementContainer.getElementById(currentInnerArc.getTargetId())!=null)
+        		{
+        			currentOuterArc = elementContainer.findArc(currentConnectedModel.getId(), 
+        					currentInnerArc.getTargetId()); 
+        		}
+        		
+        		// Always try to pass an outer arc with graphics information
+        		// (contains way points)
+//        		initArc(iNet.addNewArc(), (currentOuterArc!=null)?currentOuterArc:currentInnerArc, 
+//        				currentInnerArc);
+        		initArc(iNet.addNewArc(), currentOuterArc, 
+        				currentInnerArc);
+        	}
+        }
 
         
 	}
@@ -153,37 +249,53 @@ public class CurrentNetPnml {
     }
 	
 
-	private ArcType initArc(ArcType iArc, ArcModel outerArc)
+//	private ArcType initArc(ArcType iArc, ArcModel outerArc)
+//    {
+//		ArcModel useArc = outerArc;
+//
+//		// inscription
+//        initNodeName(iArc.addNewInscription(), useArc);
+//        
+//		// attr. id
+//        iArc.setId(useArc.getId());
+//
+//        // attr. source
+//        
+//        iArc.setSource(useArc.getSourceId());
+//        
+//        String sourceId = useArc.getSourceId();   
+//        if (sourceId.indexOf(
+//				OperatorTransitionModel.INNERID_SEPERATOR) != 0) {
+//        	sourceId = sourceId.split(OperatorTransitionModel.INNERID_SEPERATOR)[0];
+//		} 	
+//        iArc.setSource(sourceId);
+//        
+//        // attr. target
+//        String targetId = useArc.getTargetId();
+//        if (targetId.indexOf(
+//				OperatorTransitionModel.INNERID_SEPERATOR) != 0) {
+//        	targetId = targetId.split(OperatorTransitionModel.INNERID_SEPERATOR)[0];
+//		}
+//        iArc.setTarget(targetId);
+//        return iArc;
+//    }
+    
+	private ArcType initArc(ArcType iArc, ArcModel outerArc, ArcModel innerArc)
     {
-		ArcModel useArc = outerArc;
-
-		// inscription
+        ArcModel useArc = innerArc == null ? outerArc : innerArc;
+        // inscription
         initNodeName(iArc.addNewInscription(), useArc);
-        
-		// attr. id
-        iArc.setId(useArc.getId());
 
+        // attr. id
+        iArc.setId(outerArc.getId());
         // attr. source
-        
         iArc.setSource(useArc.getSourceId());
         
-        String sourceId = useArc.getSourceId();   
-        if (sourceId.indexOf(
-				OperatorTransitionModel.INNERID_SEPERATOR) != 0) {
-        	sourceId = sourceId.split(OperatorTransitionModel.INNERID_SEPERATOR)[0];
-		} 	
-        iArc.setSource(sourceId);
-        
         // attr. target
-        String targetId = useArc.getTargetId();
-        if (targetId.indexOf(
-				OperatorTransitionModel.INNERID_SEPERATOR) != 0) {
-        	targetId = targetId.split(OperatorTransitionModel.INNERID_SEPERATOR)[0];
-		}
-        iArc.setTarget(targetId);
+        iArc.setTarget(useArc.getTargetId());
+       
         return iArc;
     }
-    
     
     private ArcNameType initNodeName(ArcNameType nodeName, ArcModel element)
     {
