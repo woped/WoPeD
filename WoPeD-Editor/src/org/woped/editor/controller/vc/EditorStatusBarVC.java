1fdb66460aa1b243d78535a7633246891c41ec63
tfreytag <freytag@woped.org>
2013-01-11 10:00:30 +0100

    Improved layout of Editor status bar on Mac

--- WoPeD-Editor/src/org/woped/editor/controller/vc/EditorStatusBarVC.java
+++ WoPeD-Editor/src/org/woped/editor/controller/vc/EditorStatusBarVC.java
@@ -1,5 +1,6 @@
 package org.woped.editor.controller.vc;
 
+import java.awt.BorderLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -17,68 +18,66 @@ import javax.swing.event.ChangeListener;
 import org.woped.core.controller.IEditor;
 import org.woped.translations.Messages;
 
-
 @SuppressWarnings("serial")
 public class EditorStatusBarVC extends JPanel implements Observer {
-
+	
 	private JLabel m_counterLabel = null;
-
+	
 	private JLabel m_saveIcon = null;
-
+	
 	private EditorVC m_editor = null;
-
+	
 	private JLabel m_zoomLabel = null;
-
-	// start of adaption
-	// Saskia Kurz, Jan 2012
-
-	private JSlider zoom = null;
+	
+	private JSlider m_zoom = null;
 	
 	private JLabel m_modelingDirection = null;
-
-	// end of adaption
-
+	
 	public EditorStatusBarVC(IEditor editor) {
 		m_editor = (EditorVC) editor;
-		setLayout(new GridBagLayout());
-
-		// add description of modeling direction and icon
-		add(getModelingDirection(), new GridBagConstraints(0, 0, 1, 1, 1.0,
-				1.0, GridBagConstraints.FIRST_LINE_START, GridBagConstraints.NONE,
-				new Insets(1, 0, 0, 1), 0, 0));
-
-
-		// start of adaption
-		// Saskia Kurz, Jan 2012
-
-		// slider for zoom from 25 up to 400 percent of original size
-		zoom = new JSlider(JSlider.HORIZONTAL, 25, 400, 100);
-		zoom.setToolTipText("Zoom");	
+		JPanel bar = new JPanel();
+		bar.setLayout(new BorderLayout());
+		bar.add(getModelingDirection(), BorderLayout.WEST);
 		
-		zoom.addChangeListener(new SliderComboListener());
+		JPanel jp = new JPanel();
+		jp.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
 		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.anchor = GridBagConstraints.LINE_START;
+		c.gridx = 0;
+		c.gridy = 0;
+		c.gridwidth = 1;
+		c.insets = new Insets(0,5,5,0);
+		jp.add(getZoomLabel(), c);
 		
-		add(getZoomLabel(), new GridBagConstraints(0, 0, 1, 1, 1.0,
-				1.0, GridBagConstraints.FIRST_LINE_END, GridBagConstraints.NONE,
-				new Insets(1, 0, 0, 330), 0, 0));
-		add(zoom, new GridBagConstraints(0, 0, 1, 1, 1.0,
-				1.0, GridBagConstraints.FIRST_LINE_END, GridBagConstraints.NONE,
-				new Insets(0, 0, 0, 120), 0, 0));
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.anchor = GridBagConstraints.LINE_END;
+		c.gridx = 2;
+		c.gridy = 0;
+		c.insets = new Insets(0,5,5,0);
+		jp.add(getZoom(), c);
 		
-		// end of adaption
+		bar.add(jp, BorderLayout.CENTER);
+		bar.add(getSaveIcon(), BorderLayout.EAST);
 		
-
-		if (!(editor instanceof SubprocessEditorVC)) {
-		
-			add(getSaveIcon(), new GridBagConstraints(0, 0, 1, 1, 1.0,
-					1.0, GridBagConstraints.FIRST_LINE_END, GridBagConstraints.NONE,
-					new Insets(1, 0, 0, 2), 0, 0));
-		}
+		setLayout(new BorderLayout(5,5));
+		add(bar);
 		m_editor.registerStatusBar(this);
+		updateStatus();
 	}
 	
+	private JSlider getZoom() {
+		if (m_zoom == null) {
+			// slider for zoom from 25 up to 400 percent of original size
+			m_zoom = new JSlider(JSlider.HORIZONTAL, 25, 400, 100);
+			m_zoom.setToolTipText("Zoom");
+			m_zoom.putClientProperty("JSlider.isFilled", Boolean.TRUE);
+			m_zoom.addChangeListener(new SliderComboListener());
+		}
+		return m_zoom;
+	}
 	
-
 	private JLabel getCounterLabel() {
 		if (m_counterLabel == null) {
 			m_counterLabel = new JLabel();
@@ -86,14 +85,14 @@ public class EditorStatusBarVC extends JPanel implements Observer {
 		}
 		return m_counterLabel;
 	}
-
+	
 	private JLabel getZoomLabel() {
 		if (m_zoomLabel == null) {
 			m_zoomLabel = new JLabel();
 		}
 		return m_zoomLabel;
 	}
-
+	
 	private JLabel getSaveIcon() {
 		if (m_saveIcon == null) {
 			m_saveIcon = new JLabel();
@@ -101,8 +100,6 @@ public class EditorStatusBarVC extends JPanel implements Observer {
 		return m_saveIcon;
 	}
 	
-	// start of adaption
-	// Saskia Kurz, Jan 2012
 	private JLabel getModelingDirection(){
 		
 		if (m_modelingDirection == null){
@@ -112,55 +109,43 @@ public class EditorStatusBarVC extends JPanel implements Observer {
 	}
 	
 	// end of adaption
-
+	
 	public void updateStatus() {
 		
-		// Saskia Kurz, Jan 2012
-		// net statistics have been removed from the status bar!
-		// Saskia Kurz, Jan 2012
-		// adaption: depending on modeling direction the respective icon is added
-		
 		if (m_editor.isRotateSelected()) {
-			Messages.getString("Statusbar.Vertical");
-			
 			// set icon for vertical direction
 			getModelingDirection().setIcon(Messages.getImageIcon("Button.Vertical"));
 			getModelingDirection().setText(Messages.getString("Button.Vertical.Title"));
 			
 		} else {
-			Messages.getString("Statusbar.Horizontal");
-			
 			// set icon for horizontal direction
 			getModelingDirection().setIcon(Messages.getImageIcon("Button.Horizontal"));
 			getModelingDirection().setText(Messages.getString("Button.Horizontal.Title"));
 		}
-
-		StringBuilder builder = new StringBuilder();
-
-		getZoomLabel().setText(
-				"Zoom: "
-						+ DecimalFormat.getPercentInstance().format(
-								m_editor.getGraph().getScale()));
-
-		if (m_editor.isSaved()) {
-			getSaveIcon().setText(Messages.getString("Button.Saved.Title"));
-			getSaveIcon().setIcon(Messages.getImageIcon("Button.Saved"));
-		} else {
-			getSaveIcon().setText(Messages.getString("Button.NotSaved.Title"));
-			getSaveIcon().setIcon(Messages.getImageIcon("Button.NotSaved"));
-		}
 		
-		//Nils Lamb, Jan 2012
-		// adapted by Saskia Kurz, Feb 2012
+		StringBuilder builder = new StringBuilder();
+		
+		getZoomLabel().setText("Zoom: " + DecimalFormat.getPercentInstance().format(
+																					m_editor.getGraph().getScale()));
+		
+		if (!(m_editor instanceof SubprocessEditorVC)) {	
+			if (m_editor.isSaved()) {
+				getSaveIcon().setText(Messages.getString("Button.Saved.Title"));
+				getSaveIcon().setIcon(Messages.getImageIcon("Button.Saved"));
+			} else {
+				getSaveIcon().setText(Messages.getString("Button.NotSaved.Title"));
+				getSaveIcon().setIcon(Messages.getImageIcon("Button.NotSaved"));
+			}
+		}
 		
 		// shorten the counterlabel text when the window is too small
 		int barWidth = this.getWidth();
-		int zoomAndSaveWidth = getModelingDirection().getWidth() + getZoomLabel().getWidth() + zoom.getWidth() + getSaveIcon().getWidth()+15;
+		int zoomAndSaveWidth = getModelingDirection().getWidth() + getZoomLabel().getWidth() + getZoom().getWidth() + getSaveIcon().getWidth()+15;
 		java.awt.FontMetrics fm = getCounterLabel().getFontMetrics(getCounterLabel().getFont());
 		int counterLabelWidth = 0;
 		
 		builder.append(getModelingDirection().getText());
-			
+		
 		if(barWidth > 0 && fm.stringWidth(builder.toString()+"...") + zoomAndSaveWidth > barWidth){
 			// shorten it until it fits
 			counterLabelWidth = fm.stringWidth(builder.toString());
@@ -173,22 +158,19 @@ public class EditorStatusBarVC extends JPanel implements Observer {
 			// as text is displayed by label of an icon now, set icon text
 			getModelingDirection().setText(builder.toString());
 		}
-
+		
 	}
-
+	
 	public void update(Observable arg0, Object arg1) {
 		updateStatus();
 	}
-
-	// start of adaption
-	// Saskia Kurz, Jan 2012
-
+	
 	// listener class for JSlider (zoom functionality)
 	class SliderComboListener implements ChangeListener {
-
+		
 		JSlider slide;
 		JComboBox combo;
-
+		
 		// JSlider event
 		public void stateChanged(ChangeEvent ce) {
 			if (ce.getSource() instanceof JSlider) {
@@ -204,8 +186,4 @@ public class EditorStatusBarVC extends JPanel implements Observer {
 		}
 
 	}
-	
-
-
-	// end of adaption
 }
