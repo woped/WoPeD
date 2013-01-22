addbe0d43d50b840e59b118dae2fd348f7d848fa
tfreytag <freytag@woped.org>
2013-01-22 14:41:14 +0100

    Changes in Dialog appearing on first WoPeD start

---
+++ WoPeD-StandaloneRunner/src/org/woped/gui/AskToStartWoPeDUI.java
@@ -0,0 +1,51 @@
+package org.woped.gui;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+import org.woped.core.utilities.LoggerManager;
+import org.woped.translations.Messages;
+
+public class AskToStartWoPeDUI extends JDialog {
+	private static final long serialVersionUID = 1L;
+	
+	private JButton noButton, yesButton;
+
+	public AskToStartWoPeDUI(JFrame owner) {
+		super(owner, Messages.getString("Dialog.StartWoPeD.Text"), true);
+		this.setAlwaysOnTop(true);
+		setLayout(new BorderLayout());
+		setSize(400,300); 
+		Dimension d = this.getToolkit().getScreenSize(); 
+	    setLocation((int) ((d.getWidth() - this.getWidth()) / 2), (int) ((d.getHeight() - this.getHeight()) / 2));
+	    JPanel panel1 = new JPanel();
+	    panel1.setLayout(new BorderLayout());
+        add(new JLabel(Messages.getImageIcon("Dialog.StartWoPeD")), BorderLayout.CENTER);
+		JPanel panel2 = new JPanel();
+		panel2.setLayout(new FlowLayout(FlowLayout.CENTER));
+		yesButton = new JButton(Messages.getString("Dialog.StartWoPeD.Yes.Text"));
+        yesButton.setIcon(Messages.getImageIcon("Dialog.StartWoPeD.Yes"));
+		yesButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				setVisible(false);
+				dispose();
+			}
+		});
+		panel2.add(yesButton);
+		noButton = new JButton(Messages.getString("Dialog.StartWoPeD.No.Text"));
+        noButton.setIcon(Messages.getImageIcon("Dialog.StartWoPeD.No"));
+		noButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				LoggerManager.info(Constants.GUI_LOGGER, "EXIT APPLICATION");
+				System.exit(0);
+			}
+		});
+		panel2.add(noButton);
+		add(panel1, BorderLayout.NORTH);
+		add(panel2, BorderLayout.SOUTH);
+		pack();
+	}
+}
