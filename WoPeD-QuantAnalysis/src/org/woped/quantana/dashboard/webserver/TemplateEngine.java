53c38c0ab8fb52399bec8c1f3f4b15e9eabbe374
tfreytag <freytag@woped.org>
2015-07-29 17:18:23 +0200

    Unused import

--- WoPeD-QuantAnalysis/src/org/woped/quantana/dashboard/webserver/TemplateEngine.java
+++ WoPeD-QuantAnalysis/src/org/woped/quantana/dashboard/webserver/TemplateEngine.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
