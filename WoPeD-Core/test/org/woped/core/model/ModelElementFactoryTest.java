a1de32575a89afd66a4617426051c2167bc4b183
Andreas Danek <a.danek@live.com>
2016-08-24 16:53:14 +0200

    Add ModelElementFactory tests

--- WoPeD-Core/test/org/woped/core/model/ModelElementFactoryTest.java
+++ WoPeD-Core/test/org/woped/core/model/ModelElementFactoryTest.java
@@ -2,43 +2,34 @@ package org.woped.core.model;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.woped.core.Constants;
 import org.woped.core.utilities.ILogger;
 import org.woped.core.utilities.LoggerManager;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 public class ModelElementFactoryTest {
+
     @Test
     public void createArcModel_validId_createsInstance() throws Exception {
 
-        ModelElementFactory sut = new ModelElementFactory();
-
         ArcModel arc1 = ModelElementFactory.createArcModel("arc1", null, null);
-
         assertNotNull(arc1);
-
     }
 
     @Test
     public void createArcModel_noId_returnsNull(){
-        ModelElementFactory sut = new ModelElementFactory();
 
-
-        ArcModel expected = null;
-        ArcModel actual = ModelElementFactory.createArcModel(null, null, null);
-
-
-        assertEquals(expected, actual);
+        ArcModel arc = ModelElementFactory.createArcModel(null, null, null);
+        assertNull(arc);
     }
 
     @Test
     public void createArcModel_noId_logsError(){
-        ModelElementFactory sut = new ModelElementFactory();
 
         ILogger logger = mock(ILogger.class);
         LoggerManager.register(logger, Constants.CORE_LOGGER);
@@ -57,5 +48,4 @@ public class ModelElementFactoryTest {
     public void tearDown(){
         LoggerManager.resetForTesting();
     }
-
 }
