eabf3e6b854dcb1af6a4e43e44f3bf2a0bcb1d2a
Andreas Danek <a.danek@live.com>
2016-09-16 17:28:59 +0200

    Added tests for PNML Import

--- WoPeD-FileInterface/tests/org/woped/file/PNMLImportTest.java
+++ WoPeD-FileInterface/tests/org/woped/file/PNMLImportTest.java
@@ -13,14 +13,18 @@ import org.woped.core.model.petrinet.AbstractPetriNetElementModel;
 import org.woped.core.model.petrinet.OperatorTransitionModel;
 import org.woped.core.model.petrinet.PlaceModel;
 import org.woped.core.model.petrinet.TransitionModel;
+import org.woped.core.utilities.ILogger;
+import org.woped.core.utilities.LoggerManager;
 import org.woped.pnml.ArcType;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class PNMLImportTest {
 
     private PNMLImport sut;
+    private ILogger logger;
 
     @Before
     public void setUp() throws Exception {
@@ -29,11 +33,17 @@ public class PNMLImportTest {
         sut = new PNMLImport(mediator);
 
         ConfigurationManager.setConfiguration(ConfigurationManager.getStandardConfiguration());
+
+        logger = mock(ILogger.class);
+        LoggerManager.resetForTesting();
+        LoggerManager.register(logger, Constants.FILE_LOGGER);
     }
 
     @After
     public void tearDown() throws Exception {
         ConfigurationManager.setConfiguration(ConfigurationManager.getStandardConfiguration());
+
+        LoggerManager.resetForTesting();
     }
 
     @Test
@@ -44,6 +54,7 @@ public class PNMLImportTest {
         arcs[0].setId("a1");
         arcs[0].setSource("p1");
         arcs[0].setTarget("t1");
+        arcs[0].addNewInscription().setText(2);
 
         DemoNet net = new DemoNet();
 
@@ -70,6 +81,7 @@ public class PNMLImportTest {
         arcs[0].setId("a1");
         arcs[0].setSource("p1");
         arcs[0].setTarget("t1");
+        arcs[0].addNewInscription().setText(2);
 
         DemoNet net = new DemoNet();
 
@@ -212,7 +224,7 @@ public class PNMLImportTest {
         ConfigurationManager.getConfiguration().setImportToolspecific(true);
         sut.importArcs(arcs, net.container);
 
-        String expectedWarning = "- ARC LOST INFORMATION (a1): Exception while importing lesser important " + "information.";
+        String expectedWarning = "- INVALID ARC (a1): Source is not a valid operator.";
         assertTrue(sut.warnings.contains(expectedWarning));
     }
 
@@ -230,7 +242,7 @@ public class PNMLImportTest {
         ConfigurationManager.getConfiguration().setImportToolspecific(true);
         sut.importArcs(arcs, net.container);
 
-        String expectedWarning = "- ARC LOST INFORMATION (a1): Exception while importing lesser important " + "information.";
+        String expectedWarning = "- INVALID ARC (a1): Target is not a valid operator.";
         assertTrue(sut.warnings.contains(expectedWarning));
     }
 
@@ -252,13 +264,72 @@ public class PNMLImportTest {
         assertFalse(sut.warnings.contains(expectedWarning));
     }
 
+    @Test
+    public void importArcs_toolSpecificSourceAndTargetAreInnerElements_skipsArc() throws Exception {
+        ArcType[] arcs = new ArcType[1];
+
+        arcs[0] = ArcType.Factory.newInstance();
+        arcs[0].setId("a21");
+        arcs[0].setSource("P_CENTER_t2");
+        arcs[0].setTarget("t2_op_1");
+
+        DemoNet net = new DemoNet();
+        CreationMap map = CreationMap.createMap();
+        map.setId("t2");
+        map.setType(AbstractPetriNetElementModel.TRANS_OPERATOR_TYPE);
+        map.setOperatorType(OperatorTransitionModel.XOR_SPLITJOIN_TYPE);
+        TransitionModel t2 = (TransitionModel) ModelElementFactory.createModelElement(map);
+        net.container.addElement(t2);
+
+        ConfigurationManager.getConfiguration().setImportToolspecific(true);
+        sut.importArcs(arcs, net.container);
+
+        verify(logger).debug(" ... Arc skipped: (ID:" + arcs[0].getId() + ") " + arcs[0].getSource() + " -> " + arcs[0].getTarget() + " ");
+    }
+
+    @Test
+    public void getParentId_containsPlaceSeparator_returnsTransitionId() throws Exception {
+        String childId = OperatorTransitionModel.OPERATOR_SEPERATOR_PLACE + "t3";
+        String expected = "t3";
+        String actual = sut.getParentId(childId);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getParentId_containsTransitionSeparator_returnsTransitionId() throws Exception {
+        String childId = "t1" + OperatorTransitionModel.OPERATOR_SEPERATOR_TRANSITION + "1";
+        String expected = "t1";
+        String actual = sut.getParentId(childId);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getParentId_containsOldSeparator_returnsTransitionId() throws Exception {
+        String childId = "t1" + OperatorTransitionModel.INNERID_SEPERATOR_OLD + "1";
+        String expected = "t1";
+        String actual = sut.getParentId(childId);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getParentId_containsNoSeparator_returnsNull() throws Exception {
+        String childId = "t1";
+        String actual = sut.getParentId(childId);
+
+        assertNull(actual);
+    }
+
+
     private class DemoNet {
 
         ModelElementContainer container;
         PlaceModel p1;
         TransitionModel t1;
 
-        public DemoNet() {
+        DemoNet() {
             container = new ModelElementContainer();
 
             CreationMap placeMap = CreationMap.createMap();
