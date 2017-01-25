f6fe31ed6921418527d87c5c33e27ad37672dc4b
Andreas Danek <a.danek@live.com>
2017-01-25 17:43:06 +0100

    Reviewed circular layout for coverability graphs

--- WoPeD-QualAnalysis/src/org/woped/qualanalysis/coverabilitygraph/gui/layout/circular/CircleCoordinates.java
+++ WoPeD-QualAnalysis/src/org/woped/qualanalysis/coverabilitygraph/gui/layout/circular/CircleCoordinates.java
@@ -1,4 +1,4 @@
-package org.woped.qualanalysis.reachabilitygraph.gui.layout;
+package org.woped.qualanalysis.coverabilitygraph.gui.layout.circular;
 
 import java.awt.Point;
 import java.util.LinkedList;
@@ -6,7 +6,7 @@ import java.util.LinkedList;
  * Class for getting the Coordinates for a number of objects on a circle
  *
  */
-public class CircleCoordinates {
+class CircleCoordinates {
 	/**
 	 * Method to get the Coordinates on a circle with a certain width and height for a number of objects
 	 * @param width of the graph window
@@ -14,20 +14,21 @@ public class CircleCoordinates {
 	 * @param count of the objects to be displayed in the window
 	 * @return LinkedList containing a corner point for each object
 	 */
-	public static LinkedList<Point> getCircleCoordinates(int width, int height, int objectcount) {
-		LinkedList<Point> points = new LinkedList<Point>();
+	@SuppressWarnings("SpellCheckingInspection")
+	static LinkedList<Point> getCircleCoordinates(int width, int height, int count) {
+		LinkedList<Point> points = new LinkedList<>();
 		// get the Middle of the graph and do some correction to fit correctly
 		int mwidth = (width / 2) - 60;
 		int mheight = (height / 2) - 45;
 
 		// get the steps in degrees
 		double degreestep = 0;
-		if(objectcount > 0){
-			degreestep = 360 / objectcount;	
+		if(count > 0){
+			degreestep = 360 / count;
 		}
 		
 		//Fill the linked list with coordinates for every object
-		for(int i = 0; i < objectcount; i++) {
+		for(int i = 0; i < count; i++) {
 			int x = (int) (mwidth + (mwidth * (Math.sin(Math.toRadians(i * degreestep)))) + 5);
 			int y = (int) (mheight - (mheight * (Math.cos(Math.toRadians(i * degreestep)))) + 5);
 			points.add(new Point(x, y));
