305b1aa213993c3b034af2f7b94d76b4f3adc81a
Andreas Danek <a.danek@live.com>
2017-01-25 17:06:58 +0100

    Cleanup

--- WoPeD-QualAnalysis/src/org/woped/qualanalysis/reachabilitygraph/controller/SimulationRunningException.java
+++
@@ -1,7 +0,0 @@
-package org.woped.qualanalysis.reachabilitygraph.controller;
-
-public class SimulationRunningException extends Exception {
-
-	private static final long serialVersionUID = 5579187563319301949L;
-	
-}
--- WoPeD-QualAnalysis/src/org/woped/qualanalysis/reachabilitygraph/gui/layout/CoverabilityGraphLayout.java
+++
@@ -1,7 +0,0 @@
-package org.woped.qualanalysis.reachabilitygraph.gui.layout;
-
-public enum CoverabilityGraphLayout {
-    HIERARCHIC,
-    CIRCULAR,
-    TREE
-}
