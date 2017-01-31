83d57e4850f9cf7d3145a48bd3ad32f25de27973
Andreas Danek <a.danek@live.com>
2017-01-31 08:25:28 +0100

    Fixed eclipse build path

--- WoPeD-UnitTests/tests/org/woped/qualanalysis/coverabilitygraph/gui/AllTestsQualAnalysisCoverabilityGraphGui.java
+++ WoPeD-UnitTests/tests/org/woped/qualanalysis/coverabilitygraph/gui/AllTestsQualAnalysisCoverabilityGraphGui.java
@@ -4,6 +4,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({CoverabilityGraphRibbonMenuTest.class})
+@Suite.SuiteClasses({CoverabilityGraphRibbonMenuTest.class, CoverabilityGraphFrameControllerTest.class})
 public class AllTestsQualAnalysisCoverabilityGraphGui {
 }
