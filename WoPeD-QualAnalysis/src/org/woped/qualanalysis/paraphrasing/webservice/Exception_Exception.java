cc50a9ff32824fb8e9241f66ab37bfacc8b25cd0
freytag <freytag@woped.org>
2018-07-31 12:20:51 +0200

    Make use of new version of P2T server
    Former-commit-id: 7cee587ffc29576ee18c7e2ed25b67e2fdfc1ab4

--- WoPeD-QualAnalysis/src/org/woped/qualanalysis/paraphrasing/webservice/Exception_Exception.java
+++ WoPeD-QualAnalysis/src/org/woped/qualanalysis/paraphrasing/webservice/Exception_Exception.java
@@ -50,8 +50,5 @@ public class Exception_Exception
     public org.woped.qualanalysis.paraphrasing.webservice.Exception getFaultInfo() {
         return faultInfo;
     }
-    public void setFaultInfo(org.woped.qualanalysis.paraphrasing.webservice.Exception f) {
-        faultInfo = f;
-    }
 
 }
