f72235d83e01ab88b0f904bcf2c4be96351ad15d
ChelloX <d.semling@gmx.de>
2020-06-29 12:02:49 +0200

    Merge T2P Configuration Changes Part 2

--- WoPeD-Core/src/main/java/org/woped/core/config/DefaultStaticConfiguration.java
+++ WoPeD-Core/src/main/java/org/woped/core/config/DefaultStaticConfiguration.java
@@ -89,7 +89,7 @@ public class DefaultStaticConfiguration implements IGeneralConfiguration {
 	public static boolean DEFAULT_PROCESS2TEXT_USE = false;
 
 	public static String DEFAULT_TEXT2PROCESS_HOST = "woped.dhbw-karlsruhe.de";
-	public static int DEFAULT_TEXT2PROCESS_PORT = 8080;
+	public static int DEFAULT_TEXT2PROCESS_PORT = 8081;
 	public static String DEFAULT_TEXT2PROCESS_URI = "/t2p";
 	public static boolean DEFAULT_TEXT2PROCESS_USE = false;
 
