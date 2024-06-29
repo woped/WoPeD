dd3f2000e8aa684f48b5a20e52cba5748fc3826a
anneke02 <annekedeweerd@web.de>
2024-06-29 16:49:21 +0200

    Remove unused mediator

--- WoPeD-FileInterface/src/main/java/org/woped/file/p2t/P2TUI.java
+++ WoPeD-FileInterface/src/main/java/org/woped/file/p2t/P2TUI.java
@@ -39,7 +39,6 @@ public class P2TUI extends JDialog {
     private JCheckBox showAgainCheckBox; // New Checkbox
     private JRadioButton newRadioButton = null;
     private JRadioButton oldRadioButton = null;
-    private AbstractApplicationMediator m_mediator = null;
     JComboBox<String> modelComboBox;
 
     private static final String DEFAULT_PROMPT = "Create a clearly structured and comprehensible continuous text from the given BPMN that is understandable for an uninformed reader. The text should be easy to read in the summary and contain all important content; if there are subdivided points, these are integrated into the text with suitable sentence beginnings in order to obtain a well-structured and easy-to-read text. Under no circumstances should the output contain sub-items or paragraphs, but should cover all processes in one piece!";
